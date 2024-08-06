2b83c782e8981fcfab75e12b2490749d3a14dbf2
shemishtamesh <shemishtamail@gmail.com>
2024-08-06 23:39:19 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -16,5 +16,23 @@ in
             vibrancy = 0.2;
             vibrancy_darkness = 0.2;
         };
+        input-field = {
+            size = "250, 60";
+            outline_thickness = 2;
+            dots_size = 0.2;
+            dots_spacing = 0.35;
+            dots_center = true;
+            outer_color = "rgba(0, 0, 0, 0)";
+            inner_color = "rgba(0, 0, 0, 0.2)";
+            font_color = "rgba(255, 255, 255, 255)";
+            fade_on_empty = false;
+            rounding = -1;
+            check_color = "rgb(204, 136, 34)";
+            placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
+            hide_input = false;
+            position = "0, -200";
+            halign = "center";
+            valign = "center";
+        };
     };
 }
