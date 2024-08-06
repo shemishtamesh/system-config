72b67625ee08b17cbd976e2695b8900f6281539f
shemishtamesh <shemishtamail@gmail.com>
2024-08-06 23:52:21 +0300

    update

--- modules/apps/wlogout.nix
+++ modules/apps/wlogout.nix
@@ -6,32 +6,44 @@ in
 {
   programs.wlogout = {
     enable = true;
-    layout = [];
-      # {
-      #   label = "lock";
-      #   action = "hyprlock";
-      #   text = "lock";
-      #   keybind = "l";
-      # }
-      # {
-      #   label = "hibernate";
-      #   action = "systemctl hibernate";
-      #   text = "hibernate";
-      #   keybind = "h";
-      # }
-      # {
-      #   label = "sleep";
-      #   action = "systemctl sleep";
-      #   text = "sleep";
-      #   keybind = "s";
-      # }
-      # {
-      #   label = "shutdown";
-      #   action = "systemctl poweroff";
-      #   text = "shutdown";
-      #   keybind = "p";
-      # }
-    # ];
+    layout = [
+      {
+        label = "reboot";
+        action = "reboot";
+        text = "reboot";
+        keybind = "r";
+      }
+      {
+        label = "logout";
+        action = "logout";
+        text = "logout";
+        keybind = "o";
+      }
+      {
+        label = "lock";
+        action = "hyprlock";
+        text = "lock";
+        keybind = "l";
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "hibernate";
+        keybind = "h";
+      }
+      {
+        label = "sleep";
+        action = "systemctl sleep";
+        text = "sleep";
+        keybind = "s";
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "shutdown";
+        keybind = "p";
+      }
+    ];
     style = /* css */ ''
       * {
           background-image: none;
