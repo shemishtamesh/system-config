a5d93c7f76728c17cd003b631a2423ee6fca6253
shemishtamesh <shemishtamail@gmail.com>
2024-08-06 23:01:46 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -1,13 +1,15 @@
-{ ... }:
-
+{ pkgs, config, ... }:
+let
+  wallpaper = (import ../wallpaper.nix { inherit pkgs; inherit config; }).wallpaper;
+in
 {
     programs.hyprlock.enable = true;
     programs.hyprlock.settings = {
         general = {
             grace = 60;
         };
-        background {
-            path = ${}
+        background = {
+            path = wallpaper;
         };
     };
 }
