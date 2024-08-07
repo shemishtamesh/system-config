d404ce468efc685041c3e523a71ae7972a62e117
shemishtamesh <shemishtamail@gmail.com>
2024-08-07 23:07:05 +0300

    update

--- modules/wallpaper.nix
+++ modules/wallpaper.nix
@@ -94,7 +94,7 @@ let
       '';
       installPhase = "install -Dm0644 wallpaper.png $out";
     };
-  scheme = importYaml "${pkgs.base16-schemes}/share/themes/isotope.yaml";
+  scheme = importYaml pkgs.base16-schemes/share/themes/isotope.yaml;
 in
 {
   wallpaper = nixWallpaperFromScheme {
