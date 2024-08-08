3427b940d305cd34cf444dad1aa49bf2f1b2169c
shemishtamesh <shemishtamail@gmail.com>
2024-08-08 07:50:19 +0300

    update

--- modules/wallpaper.nix
+++ modules/wallpaper.nix
@@ -3,7 +3,7 @@ let
   importYaml =
     file: builtins.fromJSON (
       builtins.readFile (
-        pkgs.runCommandNoCC "converted-yaml.json" ''
+        pkgs.runCommandNoCC "converted-yaml.json" {} ''
           ${pkgs.yj}/bin/yj < "${file}" > "$out"
         ''
       )
