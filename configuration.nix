4e39b4dcfc09a9be75cda5dc96aa65c087fb6ff6
shemishtamesh <shemishtamail@gmail.com>
2024-08-08 23:18:01 +0300

    update

--- configuration.nix
+++ configuration.nix
@@ -170,11 +170,11 @@ in
 
   stylix.enable = true;
   stylix.base16Scheme = theme.scheme;
-  # stylix.image = theme.wallpaper;
-  stylix.image = pkgs.fetchurl {
-    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  };
+  stylix.image = theme.wallpaper;
+  # stylix.image = pkgs.fetchurl {
+  #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+  #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  # };
 
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
