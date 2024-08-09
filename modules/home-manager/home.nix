1a9bafc5496a5c88fb540a1397bf6f1ae1682ce3
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 09:44:16 +0300

    update

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -20,6 +20,16 @@ in
 
   inherit colorScheme;
 
+
+  stylix.base16Scheme = theme.scheme;
+  stylix.image = theme.wallpaper;
+  # stylix.targets.kitty.enable = true;
+  # stylix.targets.kitty.enable = false;
+  # stylix.image = pkgs.fetchurl {
+  #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+  #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  # };
+
   nixpkgs.config.allowUnfree = true;
 
   # Let Home Manager install and manage itself.
