615b82b6db0dcbcd73f636d2716cb6f0d2b7689b
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 17:22:09 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -1,5 +1,6 @@
 { pkgs, config, ... }:
 let
+  palette = config.lib.stylix.colors;
   wallpaper = (import ../utils/theming.nix { inherit pkgs; }).wallpaper;
 in
 {
@@ -22,7 +23,7 @@ in
       dots_size = 0.2;
       dots_spacing = 0.35;
       dots_center = true;
-      # outer_color = "rgba(0, 0, 0, 0)";
+      outer_color = "${palette}00";
       # inner_color = "rgba(0, 0, 0, 0.2)";
       # font_color = "rgba(255, 255, 255, 255)";
       fade_on_empty = false;
