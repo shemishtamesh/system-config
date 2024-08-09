21aa5ad8d16cb4f6d6a94952e6638680cc810488
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 19:08:43 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -31,7 +31,7 @@ in
       # check_color = "rgb(204, 136, 34)";
       # placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
       hide_input = true;
-      # position = "0, -350";
+      position = "0, 50";
       halign = "center";
       valign = "bottom";
     };
