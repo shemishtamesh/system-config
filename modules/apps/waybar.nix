abb3927c625c8e1636baa09ef1dc66b60e45a82f
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 09:38:57 +0300

    update

--- modules/apps/waybar.nix
+++ modules/apps/waybar.nix
@@ -227,7 +227,7 @@ in
 
       #mpris.paused {
           background: #${palette.base03};
-          color: #${palette.base05};
+          color: #${palette.base06};
       }
       #mpris.playing {
           padding: 1px 3px;
