07c40c0afea83ecd830100c116e90672cfc2c057
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 10:24:44 +0300

    update

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -26,8 +26,7 @@ in
   stylix.base16Scheme = theme.scheme;
   stylix.image = theme.wallpaper;
   stylix.autoEnable = true;
-  # stylix.
-  # stylix.targets.kitty.enable = true;
+  stylix.targets.kitty.enable = true;
   # stylix.targets.kitty.enable = false;
   # stylix.image = pkgs.fetchurl {
   #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
