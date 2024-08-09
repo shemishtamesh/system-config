cfa702632516658b15633dfbd53258533e702b09
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 16:27:25 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -22,13 +22,13 @@ in
       dots_size = 0.2;
       dots_spacing = 0.35;
       dots_center = true;
-      outer_color = "rgba(0, 0, 0, 0)";
-      inner_color = "rgba(0, 0, 0, 0.2)";
-      font_color = "rgba(255, 255, 255, 255)";
+      # outer_color = "rgba(0, 0, 0, 0)";
+      # inner_color = "rgba(0, 0, 0, 0.2)";
+      # font_color = "rgba(255, 255, 255, 255)";
       fade_on_empty = false;
       rounding = -1;
-      check_color = "rgb(204, 136, 34)";
-      placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
+      # check_color = "rgb(204, 136, 34)";
+      # placeholder_text = "<i><span foreground=\"##cdd6f4\">Input Password...</span></i>";
       hide_input = false;
       position = "0, -200";
       halign = "center";
@@ -37,18 +37,18 @@ in
     label = [
       {
         text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
-        color = "rgba(242, 243, 244, 0.75)";
+        # color = "rgba(242, 243, 244, 0.75)";
         font_size = 22;
-        font_family = "FiraCode";
+        # font_family = "FiraCode";
         position = "0, 300";
         halign = "center";
         valign = "center";
       }
       {
         text = "cmd[update:1000] echo \"$(date + \"%-I:%M\")\"";
-        color = "rgba(242, 243, 244, 0.75)";
+        # color = "rgba(242, 243, 244, 0.75)";
         font_size = 95;
-        font_family = "FiraCode";
+        # font_family = "FiraCode";
         position = "0, 200";
         halign = "center";
         valign = "center";
