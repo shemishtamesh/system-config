7ab8938003085fb04bfea778c0db6fba213d9d67
shemishtamesh <shemishtamail@gmail.com>
2024-08-09 08:54:37 +0300

    update

--- configuration.nix
+++ configuration.nix
@@ -170,7 +170,7 @@ in
   stylix.enable = true;
   stylix.base16Scheme = theme.scheme;
   stylix.image = theme.wallpaper;
-  # stylix.targets.kitty.enable = true;
+  stylix.targets.kitty.enable = true;
   # stylix.image = pkgs.fetchurl {
   #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
   #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
