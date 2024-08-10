6dadc117a0c535e9ba255128745fc3343c3bd5c3
shemishtamesh <shemishtamail@gmail.com>
2024-08-10 21:49:25 +0300

    update

--- modules/apps/wlogout.nix
+++ modules/apps/wlogout.nix
@@ -45,62 +45,62 @@ in
         keybind = "r";
       }
     ];
-    # style = /* css */ ''
-    #   /* * { */
-    #   /*     background-image: none; */
-    #   /*     box-shadow: none; */
-    #   /* } */
+    style = /* css */ ''
+      /* * { */
+      /*     background-image: none; */
+      /*     box-shadow: none; */
+      /* } */
 
-    #   /* window { */
-    #   /*     background-color: ${rgba "base00" "0.8"}; */
-    #   /* } */
+      /* window { */
+      /*     background-color: ${rgba "base00" "0.8"}; */
+      /* } */
 
-    #   /* button { */
-    #   /*     border-radius: 10px; */
-    #   /*     color: ${palette.base05}; */
-    #   /*     background-color: ${palette.base01}; */
-    #   /*     border-style: solid; */
-    #   /*     border-width: 0; */
-    #   /*     background-repeat: no-repeat; */
-    #   /*     background-position: center; */
-    #   /*     background-size: 25%; */
-    #   /*     margin: 2px; */
-    #   /* } */
+      /* button { */
+      /*     border-radius: 10px; */
+      /*     color: ${palette.base05}; */
+      /*     background-color: ${palette.base01}; */
+      /*     border-style: solid; */
+      /*     border-width: 0; */
+      /*     background-repeat: no-repeat; */
+      /*     background-position: center; */
+      /*     background-size: 25%; */
+      /*     margin: 2px; */
+      /* } */
 
-    #   /* button:focus, button:active, button:hover { */
-    #   /*     background-color: ${palette.base02}; */
-    #   /*     outline-style: none; */
-    #   /* } */
+      /* button:focus, button:active, button:hover { */
+      /*     background-color: ${palette.base02}; */
+      /*     outline-style: none; */
+      /* } */
 
-    #   /* #lock { */
-    #   /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/lock.svg")); */ */
-    #   /*     background-image: image(url("/home/shemishtamesh/testicons/lock.svg")); */
-    #   /* } */
-    #   /* #logout { */
-    #   /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/logout.svg")); */ */
-    #   /*     background-image: image(url("/home/shemishtamesh/testicons/logout.svg")); */
-    #   /* } */
-    #   /* #suspend { */
-    #   /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/suspend.svg")); */ */
-    #   /*     background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/suspend.png")); */
-    #   /*     /* background-image: image(url("/home/shemishtamesh/testicons/suspend.svg")); */ */
-    #   /* } */
-    #   /* #hibernate { */
-    #   /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/hibernate.svg")); */ */
-    #   /*     background-image: image(url("/home/shemishtamesh/testicons/hibernate.svg")); */
-    #   /* } */
-    #   /* /* #shutdown { */ */
-    #   /* /*     background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/shutdown.svg")); */ */
-    #   /* /*     /* background-image: image(url("/home/shemishtamesh/testicons/shutdown.svg")); */ */ */
-    #   /* /* } */ */
-    #   /* #reboot { */
-    #   /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/reboot.svg")); */ */
-    #   /*     background-image: image(url("/home/shemishtamesh/testicons/reboot.svg")); */
-    #   /*     /* background-image: image(url("/home/shemishtamesh/Downloads/hibernate.svg")); */ */
-    #   /*     /* background-image: url("/home/shemishtamesh/testicons/reboot.svg"); */ */
-    #   /*     /* background-image: url("${icons.reboot}"); */ */
-    #   /* } */
-    # '';
+      /* #lock { */
+      /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/lock.svg")); */ */
+      /*     background-image: image(url("/home/shemishtamesh/testicons/lock.svg")); */
+      /* } */
+      /* #logout { */
+      /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/logout.svg")); */ */
+      /*     background-image: image(url("/home/shemishtamesh/testicons/logout.svg")); */
+      /* } */
+      #suspend {
+          /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/suspend.svg")); */
+          background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/suspend.png"));
+          /* background-image: image(url("/home/shemishtamesh/testicons/suspend.svg")); */
+      }
+      /* #hibernate { */
+      /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/hibernate.svg")); */ */
+      /*     background-image: image(url("/home/shemishtamesh/testicons/hibernate.svg")); */
+      /* } */
+      /* /* #shutdown { */ */
+      /* /*     background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/shutdown.svg")); */ */
+      /* /*     /* background-image: image(url("/home/shemishtamesh/testicons/shutdown.svg")); */ */ */
+      /* /* } */ */
+      /* #reboot { */
+      /*     /* background-image: image(url("${pkgs.wlogout}/share/wlogout/assets/reboot.svg")); */ */
+      /*     background-image: image(url("/home/shemishtamesh/testicons/reboot.svg")); */
+      /*     /* background-image: image(url("/home/shemishtamesh/Downloads/hibernate.svg")); */ */
+      /*     /* background-image: url("/home/shemishtamesh/testicons/reboot.svg"); */ */
+      /*     /* background-image: url("${icons.reboot}"); */ */
+      /* } */
+    '';
   };
 }
 
