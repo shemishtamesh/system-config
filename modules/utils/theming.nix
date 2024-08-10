0d71a8d586951da234b8926528854cbed5080ed1
shemishtamesh <shemishtamail@gmail.com>
2024-08-10 13:17:34 +0300

    update

--- modules/utils/theming.nix
+++ modules/utils/theming.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 let
   functions = import ./functions.nix { inherit pkgs; };
-  scheme = functions.importYaml "${pkgs.base16-schemes}/share/themes/bright.yaml";
+  scheme = functions.importYaml "${pkgs.base16-schemes}/share/themes/irblack.yaml";
 in
 {
   inherit scheme;
