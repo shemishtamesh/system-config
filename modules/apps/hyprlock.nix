b0f0a119c6cad44a668cf2cea38d4b13b33dd368
shemishtamesh <shemishtamail@gmail.com>
2024-08-10 11:16:57 +0300

    update

--- modules/apps/hyprlock.nix
+++ modules/apps/hyprlock.nix
@@ -28,78 +28,26 @@ in
       check_color = palette.base09;
       fail_color = palette.base08;
       halign = "center";
-      valign = "center";
-      position = "0, 7";
+      valign = "top";
+      position = "0, 70";
     };
     label = [
       {
-        text = "cmd[update:1000] echo $(date)";
-        color = rgba "base02" "0.91";
+        text = "cmd[update:1000] date";
+        color = rgba "base02" "1";
         font_size = 22;
-        position = "0, 70";
+        position = "0, -70";
         halign = "center";
         valign = "bottom";
       }
+      # {
+      #   text = "cmd[update:10000] acpi";
+      #   color = rgba "base02" "1";
+      #   font_size = 14;
+      #   position = "0, -40";
+      #   halign = "center";
+      #   valign = "bottom";
+      # }
     ];
-
-    # image {
-    #     path = /home/justin/Pictures/profile_pictures/justin_square.png
-    #     size = 100
-    #     border_size = 2
-    #     border_color = $foreground
-    #     position = 0, -100
-    #     halign = center
-    #     valign = center
-    # };
-    # image {
-    #     path = /home/justin/Pictures/profile_pictures/hypr.png
-    #     size = 75
-    #     border_size = 2
-    #     border_color = $foreground
-    #     position = -50, 50
-    #     halign = right
-    #     valign = bottom
-    # };
-    # label {
-    #     monitor =
-    #     text = cmd[update:1000] echo "$(/home/justin/Documents/Scripts/whatsong.sh)" 
-    #     color = $foreground
-    #     #color = rgba(255, 255, 255, 0.6)
-    #     font_size = 18
-    #     font_family = Metropolis Light, Font Awesome 6 Free Solid
-    #     position = 0, 50
-    #     halign = center
-    #     valign = bottom
-    # };
-    # label {
-    #     monitor =
-    #     text = cmd[update:1000] echo "$(/home/justin/Documents/Scripts/whoami.sh)"
-    #     color = $foreground
-    #     font_size = 14
-    #     font_family = JetBrains Mono
-    #     position = 0, -10
-    #     halign = center
-    #     valign = top
-    # };
-    # label {
-    #     monitor =
-    #     text = cmd[update:1000] echo "$(/home/justin/Documents/Scripts/battery.sh)"
-    #     color = $foreground
-    #     font_size = 24
-    #     font_family = JetBrains Mono
-    #     position = -90, -10
-    #     halign = right
-    #     valign = top
-    # };
-    # label {
-    #     monitor =
-    #     text = cmd[update:1000] echo "$(/home/justin/Documents/Scripts/network-status.sh)"
-    #     color = $foreground
-    #     font_size = 24
-    #     font_family = JetBrains Mono
-    #     position = -20, -10
-    #     halign = right
-    #     valign = top
-    # };
   };
 }
