579c5d8a8fffa578e0f367b4ac62054206a1487f
shemishtamesh <shemishtamail@gmail.com>
2024-08-11 12:36:22 +0300

    update

--- modules/general/scripts.nix
+++ modules/general/scripts.nix
@@ -26,22 +26,22 @@
       }
 
       procmsg() {
-        if [[ "${1}" =~ member=Notify$ ]]; then
+        if [[ "''${1}" =~ member=Notify$ ]]; then
           STATE=on
           MSGTIME=$(date '+%Y-%m-%d %H:%M:%S')
           MSGBUF=()
-        elif [[ "${1}" =~ member=NotificationClosed$ ]]; then
+        elif [[ "''${1}" =~ member=NotificationClosed$ ]]; then
           STATE=off
           printbuf
         else
           if [[ "''${STATE}" == "on" ]]; then
-            if [[ "${1}" =~ ^string ]]; then
-              case "${1}" in
+            if [[ "''${1}" =~ ^string ]]; then
+              case "''${1}" in
                 "string \"\"") ;;
                 "string \"urgency\"") ;;
                 "string \"sender-pid\"") ;;
                 *)
-                  MSGBUF+=$( echo -n "${1}," )
+                  MSGBUF+=$( echo -n "''${1}," )
                 ;;
               esac
             fi
