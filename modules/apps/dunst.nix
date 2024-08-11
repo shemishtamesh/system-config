41e1ab13f6c3adbb9313743113100b6136276b98
shemishtamesh <shemishtamail@gmail.com>
2024-08-11 11:47:41 +0300

    update

--- modules/apps/dunst.nix
+++ modules/apps/dunst.nix
@@ -4,19 +4,14 @@
   services.dunst.enable = true;
   services.dunst.settings = {
     global = {
-      offset = "8x8";
+      offset = "6x8";
       separator_height = 2;
       padding = 10;
-      # horizontal_padding = 12;
-      # text_icon_padding = 12;
       frame_width = 2;
-      # separator_color = "frame";
       idle_threshold = 120;
-      # font = "JetBrainsMono Nerdfont 12";
       line_height = 0;
-      # format = "<b>%s</b>\n%b";
       alignment = "center";
-      icon_position = "off";
+      icon_position = "right";
       # startup_notification = "false";
       corner_radius = 10;
 
