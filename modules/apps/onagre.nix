fef34c02a227b28cadb0ace16556b7de917516f1
shemishtamesh <shemishtamail@gmail.com>
2024-08-11 16:17:32 +0300

    update

--- modules/apps/onagre.nix
+++ modules/apps/onagre.nix
@@ -41,32 +41,32 @@ in
             /* --align-x: left; */
             /* --align-y: center; */
 
-            .plugin-hint {
-              background: #3e6cce;
-              color: #2c2525;
-              border-color: #ffffff;
-              border-radius: 0%;
-              border-width: 0px;
-              padding: 0;
-              --width: shrink;
-              --height: fill;
-            }
+            /* .plugin-hint { */
+            /*   background: #3e6cce; */
+            /*   color: #2c2525; */
+            /*   border-color: #ffffff; */
+            /*   border-radius: 0%; */
+            /*   border-width: 0px; */
+            /*   padding: 0; */
+            /*   --width: shrink; */
+            /*   --height: fill; */
+            /* } */
 
-            .input {
-              background: #ffffff;
-              color: #2c2525;
-              border-color: #ffffff;
-              border-radius: 0%;
-              border-width: 0px;
-              padding: 0;
-              --width: fill;
-              --height: fill;
-              --text-width: fill;
-              --selection-color: #1664F5A3;
-              --placeholder-color: #37578FA3;
-              --align-x: left;
-              --align-y: center;
-            }
+            /* .input { */
+            /*   background: #ffffff; */
+            /*   color: #2c2525; */
+            /*   border-color: #ffffff; */
+            /*   border-radius: 0%; */
+            /*   border-width: 0px; */
+            /*   padding: 0; */
+            /*   --width: fill; */
+            /*   --height: fill; */
+            /*   --text-width: fill; */
+            /*   --selection-color: #1664F5A3; */
+            /*   --placeholder-color: #37578FA3; */
+            /*   --align-x: left; */
+            /*   --align-y: center; */
+            /* } */
           }
 
           .rows {
