7898eafdf66ff78852b7cbdeb840f49879eb556d
shemishtamesh <shemishtamail@gmail.com>
2024-08-11 14:06:29 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -1,16 +1,26 @@
 { pkgs, ... }:
 
 {
-    # stylix.targets.rofi.enable = false;
-    programs.rofi.enable = true;
-    programs.rofi = {
-        package = pkgs.rofi-wayland;
-        extraConfig = {
-            show-icons = true;
-            modi = "drun,run,window,emoji,calc";
-            sidebar-mode = true;
-        };
+  # stylix.targets.rofi.enable = false;
+  programs.rofi.enable = true;
+  programs.rofi = {
+    package = pkgs.rofi-wayland;
+    extraConfig = {
+      show-icons = true;
+      modi = "drun,run,window,emoji,calc";
+      sidebar-mode = true;
     };
+    plugins = with pkgs; [ rofi-calc rofi-calc ]
+    # plugins = with pkgs; [
+    #   # HACK: temporary fix until ABI update
+    #   (rofi-emoji.override {
+    #     rofi-unwrapped = rofi-wayland-unwrapped;
+    #   })
+    #   (rofi-calc.override {
+    #     rofi-unwrapped = rofi-wayland-unwrapped;
+    #   })
+    # ];
+  };
 }
 
 # configuration {
