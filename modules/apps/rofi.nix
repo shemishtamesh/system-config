191bff441f62626187a8b0be7b4aa0ff92f334f1
shemishtamesh <shemishtamail@gmail.com>
2024-08-12 22:20:07 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -7,7 +7,7 @@
     extraConfig = {
       show-icons = true;
       # modi = "combi,drun,run,ssh,keys,recursivebrowser,filebrowser";
-      modi = "combi,drun,run,ssh,keys,recursivebrowser,filebrowser,emoji:rofimoji";
+      # modi = "combi,drun,run,ssh,keys,recursivebrowser,filebrowser,emoji:rofimoji";
       modi = "combi,drun,run,ssh,keys,recursivebrowser,filebrowser,emoji:rofimoji,nerdfont:rofimoji -f nerd_font.csv";
       # modi = "combi,drun,run,ssh,keys,recursivebrowser,filebrowser,emoji,calc";
       sidebar-mode = true;
