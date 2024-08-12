e4e644b36eb6a886f993a21ae9cda5a7d93b895f
shemishtamesh <shemishtamail@gmail.com>
2024-08-12 21:39:02 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -9,15 +9,15 @@
       modi = "combi,drun,run,ssh,keys,recursivefilebrowser,filebrowser,emoji,calc";
       sidebar-mode = true;
     };
-    # plugins = with pkgs; [ rofi-calc rofi-calc ];
-    plugins = with pkgs; [
-      # HACK: temporary fix until ABI update
-      (rofi-emoji.override {
-        rofi-unwrapped = rofi-wayland-unwrapped;
-      })
-      (rofi-calc.override {
-        rofi-unwrapped = rofi-wayland-unwrapped;
-      })
-    ];
+    plugins = with pkgs; [ rofi-calc rofi-calc ];
+    # plugins = with pkgs; [
+    #   # HACK: temporary fix until ABI update
+    #   (rofi-emoji.override {
+    #     rofi-unwrapped = rofi-wayland-unwrapped;
+    #   })
+    #   (rofi-calc.override {
+    #     rofi-unwrapped = rofi-wayland-unwrapped;
+    #   })
+    # ];
   };
 }
