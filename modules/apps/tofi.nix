016a868caae9f24c5e58d5d2af713e2e088db8aa
shemishtamesh <shemishtamail@gmail.com>
2024-08-12 21:19:35 +0300

    update

--- modules/apps/tofi.nix
+++ modules/apps/tofi.nix
@@ -4,12 +4,12 @@
   programs.tofi.enable = true;
   programs.tofi.settings = {
     border-width = 0;
+    outline-width = 0;
     width = "100%";
     height = "100%";
+    padding-left = "35%";
+    padding-top = "35%";
     # num-results = 5;
-    # outline-width = 0;
-    # padding-left = "35%";
-    # padding-top = "35%";
     # result-spacing = 25;
   };
 }
