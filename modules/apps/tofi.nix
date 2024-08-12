29df333567e696cc55126da6fff330f141259cb2
shemishtamesh <shemishtamail@gmail.com>
2024-08-12 21:17:37 +0300

    update

--- modules/apps/tofi.nix
+++ modules/apps/tofi.nix
@@ -3,15 +3,13 @@
 {
   programs.tofi.enable = true;
   programs.tofi.settings = {
-    # background-color = "#000000";
-    # border-width = 0;
-    # font = "monospace";
-    # height = "100%";
+    border-width = 0;
+    width = "100%";
+    height = "100%";
     # num-results = 5;
     # outline-width = 0;
     # padding-left = "35%";
     # padding-top = "35%";
     # result-spacing = 25;
-    # width = "100%";
   };
 }
