2c7afb8da0d48f571e7f414ec3f6d4d1830a39b6
shemishtamesh <shemishtamail@gmail.com>
2024-08-13 08:58:07 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -6,26 +6,14 @@
     package = pkgs.rofi-wayland;
     extraConfig = {
       show-icons = true;
-      modi = "drun,calc";
-      # modi = "drun,window,recursivebrowser,calc,󰻐:rofimoji -f emoji*.csv math.csv nerd_font.csv";
-      # modi = "combi,drun,window,recursivebrowser,󰻐:rofimoji -f emoji*.csv math.csv nerd_font.csv";
-      # combi-modi = "drun,window,recursivebrowser,calc,󰻐:rofimoji -f emoji*.csv math.csv nerd_font.csv";
+      # modi = "drun,calc";
+      modi = "combi,drun,window,recursivebrowser,calc,󰻐:rofimoji -f emoji*.csv math.csv nerd_font.csv";
+      combi-modi = "drun,window,recursivebrowser,calc,󰻐:rofimoji -f emoji*.csv math.csv nerd_font.csv";
       # modi = "combi,drun,window,recursivebrowser,󰻐:rofimoji -f all";
-      combi-modi = "drun,calc";
+      # combi-modi = "drun,calc";
       sidebar-mode = true;
     };
     plugins = with pkgs; [ rofi-calc ];
-    # plugins = with pkgs; [ rofi-calc rofi-emoji ];
-    # plugins = with pkgs; [ rofimoji ];
-    # plugins = with pkgs; [
-    #   # HACK: temporary fix until ABI update
-    #   (rofi-emoji.override {
-    #     rofi-unwrapped = rofi-wayland-unwrapped;
-    #   })
-    #   (rofi-calc.override {
-    #     rofi-unwrapped = rofi-wayland-unwrapped;
-    #   })
-    # ];
   };
   nixpkgs.overlays = [
     (final: prev: {
