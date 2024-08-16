9b7794e522f11608fbab77c01dd0b8ff19af2ee9
shemishtamesh <shemishtamail@gmail.com>
2024-08-16 14:36:38 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -12,8 +12,8 @@ in
     extraConfig = {
       show-icons = true;
       modi = "combi,drun,window,recursivebrowser,calc,char:rofimoji --use-icons -a=copy -f emoji*.csv math.csv nerd_font.csv";
-      combi-modi = "drun,window,recursivebrowser,calc,char:rofimoji --use-icons -a=copy -f emoji*.csv math.csv nerd_font.csv";
-      # combi-modi = ":drun,window,recursivebrowser,calc,char:rofimoji --use-icons -a=copy -f emoji*.csv math.csv nerd_font.csv";
+      # combi-modi = "drun,window,recursivebrowser,calc,char:rofimoji --use-icons -a=copy -f emoji*.csv math.csv nerd_font.csv";
+      combi-modi = "󰌧:drun,window,recursivebrowser,:calc,󰻐:rofimoji --use-icons -a=copy -f emoji*.csv math.csv nerd_font.csv";
       sidebar-mode = true;
     };
     plugins = with pkgs; [ rofi-calc ];
