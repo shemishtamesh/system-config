de6ecc52fd5106d0167ba93c69fcd51146a7f422
shemishtamesh <shemishtamail@gmail.com>
2024-08-16 14:19:33 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -27,24 +27,6 @@ in
         fg0 = mkLiteral palette.base07;
         fg1 = mkLiteral (rgba "base07" 0.8);
 
-        # bg0 = mkLiteral "#242424";
-        # bg1 = mkLiteral "#363636";
-        # bg2 = mkLiteral "#f5f5f5";
-        # bg3 = mkLiteral "#f5f5f5";
-        # bg4 = mkLiteral "#0860f2";
-
-        # fg0 = mkLiteral "#f5f5f5";
-        # fg1 = mkLiteral "#f5f5f5";
-
-        # bg0 = mkLiteral "#24242480";
-        # bg1 = mkLiteral "#363636";
-        # bg2 = mkLiteral "#f5f5f520";
-        # bg3 = mkLiteral "#f5f5f540";
-        # bg4 = mkLiteral "#0860f2E6";
-
-        # fg0 = mkLiteral "#f5f5f5";
-        # fg1 = mkLiteral "#f5f5f580";
-
         background-color = mkLiteral "transparent";
         text-color = mkLiteral "@fg0";
         padding = mkLiteral "0px";
@@ -93,7 +75,7 @@ in
       listview = {
         margin = mkLiteral "48px calc( 50% - 560px )";
         spacing = mkLiteral "48px";
-        columns = 6;
+        columns = 8;
       };
 
       "element, element-text, element-icon" = {
@@ -125,20 +107,6 @@ in
   nixpkgs.overlays = [
     (final: prev: {
       rofi-calc = prev.rofi-calc.override { rofi-unwrapped = prev.rofi-wayland-unwrapped; };
-    })
-    (final: prev: {
-      # see https://github.com/svenstaro/rofi-calc/issues/117
-      libqalculate = prev.libqalculate.overrideAttrs (_: rec {
-        pname = "libqalculate";
-        version = "4.8.1";
-
-        src = pkgs.fetchFromGitHub {
-          owner = "qalculate";
-          repo = "libqalculate";
-          rev = "v${version}";
-          sha256 = "sha256-4WqKlwVf4/ixVr98lPFVfNL6EOIfHHfL55xLsYqxkhY=";
-        };
-      });
     })
   ];
 }
