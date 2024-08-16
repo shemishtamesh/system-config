44db2b2d270c651aa979337b0b594073a4bf8eb7
shemishtamesh <shemishtamail@gmail.com>
2024-08-16 18:40:35 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -73,7 +73,7 @@ in
       };
 
       listview = {
-        margin = mkLiteral "48px calc( 50% - 560px )";
+        margin = mkLiteral "48px calc( 50% - 700px )";
         spacing = mkLiteral "48px";
         columns = 8;
       };
