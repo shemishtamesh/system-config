b2dcfc8704f7fd8e19e844ee13338bcc19cdc7a6
shemishtamesh <shemishtamail@gmail.com>
2024-08-16 07:29:19 +0300

    update

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -24,9 +24,9 @@
       in
       {
         "*" = {
-          background-color = mkLiteral "#000000";
-          foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
-          border-color = mkLiteral "#FFFFFF";
+          # background-color = mkLiteral "#000000";
+          # foreground-color = mkLiteral "rgba ( 250, 251, 252, 100 % )";
+          # border-color = mkLiteral "#FFFFFF";
           width = 512;
         };
 
@@ -38,7 +38,7 @@
           expand = false;
           str = ":";
           margin = mkLiteral "0px 0.3em 0em 0em";
-          text-color = mkLiteral "@foreground-color";
+          # text-color = mkLiteral "@foreground-color";
         };
       };
   };
