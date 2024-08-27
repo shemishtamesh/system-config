9fbe26e10cb27f025ef136b5a75e91106716a7ec
shemishtamesh <shemishtamail@gmail.com>
2024-08-27 10:30:35 +0300

    update

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -71,7 +71,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
