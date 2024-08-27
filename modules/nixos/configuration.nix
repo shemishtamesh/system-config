d592dbd7422557bca4d624eb560d72157b1ef2ce
shemishtamesh <shemishtamail@gmail.com>
2024-08-27 15:00:36 +0300

    update

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -218,20 +218,17 @@
       export KEYTIMEOUT=1
       # Change cursor shape for different vi modes.
       function zle-keymap-select {
-      if [[ ''${KEYMAP} == vicmd ]] ||
-          [[ $1 = 'block' ]]; then
+        if [[ ''${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
           echo -ne '\e[1 q'
-      elif [[ ''${KEYMAP} == main ]] ||
-          [[ ''${KEYMAP} == viins ]] ||
-          [[ ''${KEYMAP} = "" ]] ||
-          [[ $1 = 'beam' ]]; then
+        elif [[ ''${KEYMAP} == main ]] || [[ ''${KEYMAP} == viins ]] \
+          || [[ ''${KEYMAP} = "" ]] || [[ $1 = 'beam' ]]; then
           echo -ne '\e[5 q'
-      fi
+        fi
       }
       zle -N zle-keymap-select
       zle-line-init() {
-      zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-      echo -ne "\e[5 q"
+        zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+        echo -ne "\e[5 q"
       }
       zle -N zle-line-init
       echo -ne '\e[5 q' # Use beam shape cursor on startup.
