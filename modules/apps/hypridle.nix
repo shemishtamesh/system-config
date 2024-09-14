fe194bf8f991abd524be5a6c092a9744e1d3bfdf
shemishtamesh <shemishtamail@gmail.com>
2024-09-14 11:58:21 +0300

    rebuilding nixos

--- modules/apps/hypridle.nix
+++ modules/apps/hypridle.nix
@@ -10,31 +10,31 @@
     };
     listener = [
       {
-        timeout = 15; # 2.5min.
+        timeout = 150; # 2.5min.
         on-timeout = "brightnessctl -s set 1%"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
         on-resume = "brightnessctl -r"; # monitor backlight restore.
       }
 
       # turn off keyboard backlight
       {
-        timeout = 15; # 2.5min.
+        timeout = 150; # 2.5min.
         on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
         on-resume = "brightnessctl -rd rgb:kbd_backlight"; # turn on keyboard backlight.
       }
 
       {
-        timeout = 30; # 5min
+        timeout = 300; # 5min
         on-timeout = "loginctl lock-session"; # lock screen when timeout has passed
       }
 
       {
-        timeout = 33; # 5.5min
+        timeout = 330; # 5.5min
         on-timeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
         on-resume = "hyprctl dispatch dpms on"; # screen on when activity is detected after timeout has fired.
       }
 
       {
-        timeout = 180; # 30min
+        timeout = 1800; # 30min
         on-timeout = "systemctl suspend"; # suspend pc
       }
     ];
