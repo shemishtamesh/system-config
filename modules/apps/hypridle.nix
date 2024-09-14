68896d614fbd49f6c65bbe3795ba0e052a73388d
shemishtamesh <shemishtamail@gmail.com>
2024-09-14 11:32:44 +0300

    rebuilding nixos

--- modules/apps/hypridle.nix
+++ modules/apps/hypridle.nix
@@ -4,12 +4,13 @@
   services.hypridle.enable = true;
   services.hypridle.settings = {
     general = {
-      lock_cmd = "hyprlock";
-      before_sleep_cmd = "hyprlock";
+      lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+      before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+      after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
     };
     listener = [
       {
-        timeout = 5;
+        timeout = 500;
         on-timeout = "notify-send idle";
         on-resume = "notify-send back";
       }
