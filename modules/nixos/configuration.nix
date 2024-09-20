4a073ab67ee56bb9b13811911815edc4faaef840
shemishtamesh <shemishtamail@gmail.com>
2024-09-20 09:24:40 +0300

    rebuilding nixos

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -297,6 +297,8 @@
 
   programs.adb.enable = true;
 
+  programs.nixvim.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
