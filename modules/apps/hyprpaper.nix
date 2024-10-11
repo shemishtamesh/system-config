81dd9d4091bf63b25ece6473317ff843ded9bc2d
shemishtamesh <shemishtamail@gmail.com>
2024-10-11 12:35:03 +0300

    rebuilding nixos

--- modules/apps/hyprpaper.nix
+++ modules/apps/hyprpaper.nix
@@ -1,12 +1,3 @@
-{ pkgs, ... }:
-let
-  wallpaper = (import ../general/theming.nix { inherit pkgs; }).wallpaper;
-in
 {
   services.hyprpaper.enable = true;
-  services.hyprpaper.settings = {
-    wallpaper = [
-      "DP-1,${wallpaper}"
-    ];
-  };
 }
