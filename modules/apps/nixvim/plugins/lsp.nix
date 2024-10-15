6749c18bf920ff11c36fb6d89caa93b9a7c79d0d
shemishtamesh <shemishtamail@gmail.com>
2024-10-15 20:05:05 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/lsp.nix
+++ modules/apps/nixvim/plugins/lsp.nix
@@ -19,17 +19,8 @@ in
         };
       };
     };
-    plugins.lsp-format.enable = true;
   };
-  # keymaps = [
-  #   (keymap "n" "<leader>fo" "<cmd>Telescope oldfiles<CR>" { })
-  #   (keymap "n" "<leader>ff" "<cmd>Telescope find_files<CR>" { })
-  #   (keymap "n" "<leader>fl" "<cmd>Telescope live_grep<CR>" { })
-  #   (keymap "n" "<leader>fg" "<cmd>Telescope git_files<CR>" { })
-  #   (keymap "n" "<leader>fs" "<cmd>Telescope grep_string<CR>" { })
-  #   (keymap "n" "<leader>fc" "<cmd>Telescope command_history<CR>" { })
-  #   (keymap "n" "<leader>fq" "<cmd>Telescope quickfix<CR>" { })
-  #   (keymap "n" "<leader>fj" "<cmd>Telescope jumplist<CR>" { })
-  #   (keymap "n" "<leader>fk" "<cmd>Telescope keymaps<CR>" { })
-  # ];
+  keymaps = [
+    (keymap "n" "<leader>lf" "<cmd>lua vim.lsp.buf.format()<CR>" { })
+  ];
 }
