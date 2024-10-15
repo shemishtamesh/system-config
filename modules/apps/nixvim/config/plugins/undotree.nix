0182ad96d30c677c18ca24fbc39c73fd57205ca4
shemishtamesh <shemishtamail@gmail.com>
2024-10-15 12:49:43 +0300

    rebuilding nixos

--- modules/apps/nixvim/config/plugins/undotree.nix
+++ modules/apps/nixvim/config/plugins/undotree.nix
@@ -2,6 +2,8 @@ let
   keymap = (import ../nix_functions.nix).keymap;
 in
 {
-  programs.nixvim.plugins.undotree.enable = true;
-  keymaps = [ (keymap "n" "<leader>u" "<cmd>UndotreeToggle<CR>" { }) ];
+  programs.nixvim.plugins.undotree = {
+    enable = true;
+    keymaps = [ (keymap "n" "<leader>u" "<cmd>UndotreeToggle<CR>" { }) ];
+  };
 }
