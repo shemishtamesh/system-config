0cc237e9f6d691a66c5993424dfa4e7a824c26d2
shemishtamesh <shemishtamail@gmail.com>
2024-10-16 09:06:56 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/lsp.nix
+++ modules/apps/nixvim/plugins/lsp.nix
@@ -20,15 +20,15 @@ in {
     keymaps = [
       (keymap "n" "<leader>lf" "<cmd>lua vim.lsp.buf.format()<CR>" { })
       (keymap "n" "<leader>ln" "<cmd>lua vim.lsp.buf.rename()<CR>" { })
-      (keymap "n" "<leader>ld" "<cmd>lua vim.lsp.buf.definition()<CR>" { })
       (keymap "n" "<leader>lm" "<cmd>lua vim.lsp.buf.implementation()<CR>" { })
       (keymap "n" "<leader>li" "<cmd>lua vim.lsp.buf.incoming_calls()<CR>" { })
       (keymap "n" "<leader>lo" "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>" { })
       (keymap "n" "<leader>lr" "<cmd>lua vim.lsp.buf.references()<CR>" { })
       (keymap "n" "<leader>lh" "<cmd>lua vim.lsp.buf.signature_help()<CR>" { })
       (keymap "n" "<leader>lt" "<cmd>lua vim.lsp.buf.type_definition()<CR>" { })
-      (keymap "n" "<leader>lc" "<cmd>lua vim.lsp.buf.type_typehierarchy()<CR>" { })
+      (keymap "n" "<leader>lc" "<cmd>lua vim.lsp.buf.typehierarchy()<CR>" { })
       (keymap "n" "<leader>ls" "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>" { })
+      (keymap "n" "<leader>ld" "<cmd>lua vim.lsp.buf.definition()<CR>" { })
       (keymap "n" "gd" "<cmd>lua vim.lsp.buf.definition()<CR>" { })
 
       (keymap "n" "<leader>dl" "<cmd>lua vim.lsp.diagnostic.gget_line_diagnostics()<CR>" { })
