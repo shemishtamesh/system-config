4bfd357f553c2d71d4a09ae26708e3f1a2cc3eb9
shemishtamesh <shemishtamail@gmail.com>
2024-10-16 09:33:01 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/lsp.nix
+++ modules/apps/nixvim/plugins/lsp.nix
@@ -8,7 +8,7 @@ in {
       servers = {
         nil-ls = {
           enable = true;
-          formatting.command = [ "${pkgs.nixfmt-rfc-style}" ];
+          extraOptions.formatting.command = [ "${pkgs.nixfmt-rfc-style}" ];
         };
         pylsp.enable = true;
         rust-analyzer = {
