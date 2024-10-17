7131c136321870649757c15990c8c6c7ca02d9f8
shemishtamesh <shemishtamail@gmail.com>
2024-10-17 14:50:51 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/completions.nix
+++ modules/apps/nixvim/plugins/completions.nix
@@ -3,11 +3,14 @@
     enable = true;
     autoEnableSources = true;
     settings = {
+      enable_chat = true;
       mapping = {
         "<C-Space>" = "cmp.mapping.complete()";
         "<C-y>" = "cmp.mapping.confirm({ select = true })";
         "<C-n>" = "cmp.mapping.select_next_item()";
         "<C-p>" = "cmp.mapping.select_prev_item()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<C-b>" = "cmp.mapping.scroll_docs(-4)";
       };
       sources = [
         { name = "nvim_lsp"; }
