3cef1eb7d5bc7b35e231a1f925935a32146cb547
shemishtamesh <shemishtamail@gmail.com>
2024-10-17 13:28:50 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/completions.nix
+++ modules/apps/nixvim/plugins/completions.nix
@@ -3,12 +3,12 @@
     enable = true;
     autoEnableSources = true;
     settings = {
-      mappings = {
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-y>" = "cmp.mapping.confirm()";
-        "<C-n>" = "cmp.mapping.select_next_item()";
-        "<C-p>" = "cmp.mapping.select_prev_item()";
-      };
+      # mappings = {
+      #   "<C-Space>" = "cmp.mapping.complete()";
+      #   "<C-y>" = "cmp.mapping.confirm({ select = true })";
+      #   "<C-n>" = "cmp.mapping.select_next_item()";
+      #   "<C-p>" = "cmp.mapping.select_prev_item()";
+      # };
       sources = [
         { name = "nvim_lsp"; }
         { name = "path"; }
