093d812d08be2153480c247446a4eb8de035155a
shemishtamesh <shemishtamail@gmail.com>
2024-10-18 10:38:15 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/completions.nix
+++ modules/apps/nixvim/plugins/completions.nix
@@ -1,3 +1,19 @@
+let
+  mapping = {
+    "<C-Space>" = "cmp.mapping.complete()";
+    "<C-y>" = "cmp.mapping.confirm({ select = true })";
+    "<C-n>" = "cmp.mapping.select_next_item()";
+    "<C-p>" = "cmp.mapping.select_prev_item()";
+    "<C-f>" = "cmp.mapping.scroll_docs(4)";
+    "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+  };
+  search_options = {
+    inherit mapping;
+    sources = [
+      { name = "buffer"; }
+    ];
+  };
+in
 {
   programs.nixvim = {
     opts.completeopt = [
@@ -8,18 +24,12 @@
     plugins.cmp = {
       enable = true;
       autoEnableSources = true;
-      cmdline."/".sources = [
-        { name = "buffer"; }
-      ];
+      cmdline = {
+        "/" = search_options;
+        "?" = search_options;
+      };
       settings = {
-        mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-y>" = "cmp.mapping.confirm({ select = true })";
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-        };
+        inherit mapping;
         sources = [
           { name = "nvim_lsp"; }
           { name = "path"; }
