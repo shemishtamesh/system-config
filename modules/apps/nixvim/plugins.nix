f2260b62b1c56c743fdd14e373004a082eb43af7
shemishtamesh <shemishtamail@gmail.com>
2024-10-19 18:47:47 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins.nix
+++ modules/apps/nixvim/plugins.nix
@@ -9,11 +9,11 @@
     ./plugins/comment.nix
     ./plugins/codeium.nix
     ./plugins/zen-mode.nix
+    ./plugins/indent-blankline.nix
     # ./plugins/alpha.nix
   ];
   programs.nixvim.plugins = {
     lualine.enable = true;
-    indent-blankline.enable = true;
     nvim-colorizer.enable = true;
     tmux-navigator.enable = true;
     which-key.enable = true;
