a21b74272a6551abd9504495ce7d51609332bfbf
shemishtamesh <shemishtamail@gmail.com>
2024-10-19 13:49:36 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/zen-mode.nix
+++ modules/apps/nixvim/plugins/zen-mode.nix
@@ -1,19 +1,27 @@
+let
+  keymap = (import ./nix_functions.nix).keymap;
+in
 {
-  programs.nixvim.plugins.zen-mode = {
-    enable = true;
-    settings = {
-      window = {
-        backdrop = 1;
-        width = 80;
-        height = 1;
-        options = {
-          signcolumn = "no";
-          number = false;
-          relativenumber = false;
-          foldcolumn = "0";
-          list = false;
+  programs.nixvim = {
+    plugins.zen-mode = {
+      enable = true;
+      settings = {
+        window = {
+          backdrop = 1;
+          width = 80;
+          height = 1;
+          options = {
+            signcolumn = "no";
+            number = false;
+            relativenumber = false;
+            foldcolumn = "0";
+            list = false;
+          };
         };
       };
     };
+    keymaps = [
+      (keymap "n" "<leader>z" "ZenMode" { })
+    ];
   };
 }
