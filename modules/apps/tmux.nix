723ddd4852d5f2032363bdccc8a640e070c5e404
shemishtamesh <shemishtamail@gmail.com>
2024-10-19 22:06:45 +0300

    rebuilding nixos

--- modules/apps/tmux.nix
+++ modules/apps/tmux.nix
@@ -6,10 +6,15 @@
     escapeTime = 0;
     keyMode = "vi";
     newSession = true;
+    shortcut = " ";
     plugins = [
       pkgs.tmuxPlugins.resurrect
       pkgs.tmuxPlugins.continuum
       pkgs.tmuxPlugins.vim-tmux-navigator
     ];
+    extraConfig = # tmux
+      ''
+        set -g mouse on
+      '';
   };
 }
