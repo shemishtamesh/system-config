cfc5bebcfc81cbbb11245ff6d4ee83029df651d7
shemishtamesh <shemishtamail@gmail.com>
2024-10-19 19:13:40 +0300

    rebuilding nixos

--- modules/apps/nixvim/plugins/zen-mode.nix
+++ modules/apps/nixvim/plugins/zen-mode.nix
@@ -24,10 +24,12 @@ in
     };
     plugins.twilight.enable = true;
     keymaps = [
-      (keymap "n" "<leader>z"
-        "<cmd>ZenMode | lua vim.diagnostic.enable(not vim.diagnostic.is_enabled()) | IBLToggle<CR>"
-        { silent = true; }
-      )
+      (keymap "n" "<leader>z" (lib.concatStrings [
+        "<cmd>ZenMode"
+        " | IBLToggle"
+        " | lua vim.diagnostic.enable(not vim.diagnostic.is_enabled())"
+        "<CR>"
+      ]) { silent = true; })
     ];
   };
 }
