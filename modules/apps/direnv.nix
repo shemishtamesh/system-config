0cb3dc8f144899266b1d7f5aa0f0e18ae18e0fc6
shemishtamesh <shemishtamail@gmail.com>
2024-10-20 08:40:25 +0300

    rebuilding nixos

--- modules/apps/direnv.nix
+++ modules/apps/direnv.nix
@@ -1,13 +1,11 @@
 {
   programs.direnv.enable = true;
-  programs.zsh.initExtra = [
-    # sh
+  programs.zsh.initExtra = # sh
     ''
       # ensure compatibility tmux <-> direnv
       if [ -n "$TMUX" ] && [ -n "$DIRENV_DIR" ]; then
           unset -m "DIRENV_*"  # unset env vars starting with DIRENV_
       fi
-    ''
-  ];
+    '';
   programs.direnv.enableZshIntegration = true;
 }
