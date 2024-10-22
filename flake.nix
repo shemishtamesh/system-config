80e8894cc3ffe84a62031e0381267e8e81746ffd
shemishtamesh <shemishtamail@gmail.com>
2024-10-22 19:03:47 +0300

    update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
 
     hyprland = {
       type = "git";
-      url = "https://github.com/hyprwm/Hyprland";
+      # url = "https://github.com/hyprwm/Hyprland";
+      url = "github:hyprwm/Hyprland";
       submodules = true;
       inputs.nixpkgs.follows = "nixpkgs";
     };
