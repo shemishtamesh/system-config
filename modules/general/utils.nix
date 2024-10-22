d6f5e6f48c0f4dac50a4ab4670143a4cf5573591
shemishtamesh <shemishtamail@gmail.com>
2024-10-22 13:11:58 +0300

    rebuilding nixos

--- modules/general/utils.nix
+++ modules/general/utils.nix
@@ -39,5 +39,5 @@ in
       installPhase = "install -Dm0644 ${name}.png $out";
     };
   sync_external_monitor_brightness = # sh
-    "${ddcutil} setvcp 10 $(echo \"$(${brightnessctl} g) / $(${brightnessctl} m) * 100\" | ${bc} -l)";
+    "${ddcutil} setvcp 10 $(echo \"$(echo \"$(${brightnessctl} g) / $(${brightnessctl} m) * 100\" | ${bc} -l | ${bc}) / 1\" | ${bc})";
 }
