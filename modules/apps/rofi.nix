c1b41f239976da94d816a2882bd57e4a36ac8341
shemishtamesh <shemishtamail@gmail.com>
2024-10-23 12:27:52 +0300

    rebuilding nixos

--- modules/apps/rofi.nix
+++ modules/apps/rofi.nix
@@ -75,7 +75,7 @@ in
       listview = {
         margin = mkLiteral "48px calc( 50% - 800px )";
         spacing = mkLiteral "48px";
-        columns = 8;
+        columns = 6;
       };
 
       "element, element-text, element-icon" = {
