0b74b0f52438d921a287508b4cf7102c6ff8a890
shemishtamesh <shemishtamail@gmail.com>
2024-10-27 07:37:03 +0200

    rebuilding nixos

--- modules/home-manager/home/scripts.nix
+++ modules/home-manager/home/scripts.nix
@@ -7,20 +7,24 @@
 
       ${pkgs.nixfmt-rfc-style}/bin/nixfmt $FLAKE
 
-      git -C $FLAKE add .
-      git -C $FLAKE commit -m 'rebuilding nixos'
-      nh os switch $FLAKE
-      if [ $? -ne 0 ] ; then
-        notify-send -u critical 'nixos rebuild failed'
-        exit 1
+      if [ -z "$1" ] || [ "$1" == "os" ]; then
+        git -C $FLAKE add .
+        git -C $FLAKE commit -m 'rebuilding nixos'
+        nh os switch $FLAKE
+        if [ $? -ne 0 ] ; then
+          notify-send -u critical 'nixos rebuild failed'
+          exit 1
+        fi
       fi
 
-      git -C $FLAKE add .
-      git -C $FLAKE commit -m 'rebuilding home'
-      nh home switch $FLAKE
-      if [ $? -ne 0 ] ; then
-        notify-send -u critical 'home rebuild failed'
-        exit 1
+      if [ -z "$1" ] || [ "$1" == "home" ]; then
+        git -C $FLAKE add .
+        git -C $FLAKE commit -m 'rebuilding home'
+        nh home switch $FLAKE
+        if [ $? -ne 0 ] ; then
+          notify-send -u critical 'home rebuild failed'
+          exit 1
+        fi
       fi
 
       systemctl --user restart hyprpaper.service
@@ -29,7 +33,7 @@
         exit 1
       fi
 
-      notify-send -u low 'rebuild succeed'
+      notify-send -u low 'successfully rebuilt'
     '')
 
     (pkgs.writeShellScriptBin "bak" ''
