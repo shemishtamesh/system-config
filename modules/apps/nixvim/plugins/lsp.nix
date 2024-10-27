33e750f0426343d048ff9d094ea1c8ec3892d0f5
shemishtamesh <shemishtamail@gmail.com>
2024-10-27 08:19:39 +0200

    rebuilding home

--- modules/apps/nixvim/plugins/lsp.nix
+++ modules/apps/nixvim/plugins/lsp.nix
@@ -36,28 +36,6 @@ in
             };
           };
         };
-        # keymaps = {
-        #   lspBuf = {
-        #     "<leader>la" = "code_action";
-        #     "<leader>lf" = "format";
-        #     "<leader>ln" = "rename";
-        #     "<leader>lm" = "implementation";
-        #     "<leader>li" = "incoming_calls";
-        #     "<leader>lo" = "outgoing_calls";
-        #     "<leader>lr" = "references";
-        #     "<leader>lh" = "signature_help";
-        #     "<leader>lt" = "type_definition";
-        #     "<leader>lc" = "typehierarchy";
-        #     "<leader>ls" = "workspace_symbol";
-        #     "gd" = "definition";
-        #     "gD" = "declaration";
-        #   };
-        #   diagnostic = {
-        #     "<leader>ld" = "setqflist";
-        #     "]d" = "goto_next";
-        #     "[d" = "goto_prev";
-        #   };
-        # };
       };
     };
     keymaps = [
