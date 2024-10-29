9b6d4172e58d6180e1b4cfd730c21819b55e9a79
shemishtamesh <shemishtamail@gmail.com>
2024-10-29 09:40:39 +0200

    rebuilding home

--- modules/home-manager/home/apps/tmux.nix
+++ modules/home-manager/home/apps/tmux.nix
@@ -1,14 +1,15 @@
-{ pkgs, ... }:
+# { pkgs, ... }:
+
 {
-  programs.tmux.enable = true;
   programs.tmux = {
-    clock24 = true;
-    escapeTime = 0;
-    keyMode = "vi";
-    newSession = true;
+    enable = true;
+    # clock24 = true;
+    # escapeTime = 0;
+    # keyMode = "vi";
+    # newSession = true;
     shortcut = "Space";
     # shortcut = "a";
-    historyLimit = 5000;
+    # historyLimit = 5000;
     # plugins = [
     #   pkgs.tmuxPlugins.better-mouse-mode
     #   pkgs.tmuxPlugins.vim-tmux-navigator
