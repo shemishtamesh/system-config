46edfe4a7376205a6b2bd030fd7db2877c6d8ac0
shemishtamesh <shemishtamail@gmail.com>
2024-11-01 11:02:07 +0200

    rebuilding home

--- modules/home-manager/home/apps/tmux.nix
+++ modules/home-manager/home/apps/tmux.nix
@@ -15,11 +15,12 @@
       # pkgs.tmuxPlugins.resurrect
       # pkgs.tmuxPlugins.continuum
     ];
-    # extraConfig = # tmux
-    #   ''
-    #     bind-key -T copy-mode-vi v send-keys -X begin-selection
-    #     bind-key -T copy-mode-vi y send-keys -X copy-selection
-    #     bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
-    #   '';
+    extraConfig = # tmux
+      ''
+        set-window-option -g mode-keys vi
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi y send-keys -X copy-selection
+        bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
+      '';
   };
 }
