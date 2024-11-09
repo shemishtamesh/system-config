7921ecb2abe1f1235859e14158c30bc0252deceb
shemishtamesh <shemishtamail@gmail.com>
2024-11-09 17:14:57 +0200

    rebuilding home

--- modules/home-manager/home/apps/tmux.nix
+++ modules/home-manager/home/apps/tmux.nix
@@ -56,7 +56,7 @@ in
 
             # set -g @resurrect-strategy-nvim 'session'
             # set -g @resurrect-processes '~nvim->nvim btop'
-            set -g @resurrect-processes 'n->nvim btop'
+            set -g @resurrect-processes '/nix/store/wkg6wm72k8sza0sbs9803gfgzwv53czn-neovim-unwrapped-0.10.2/bin/nvim->nvim btop'
             # set -g @resurrect-processes 'btop'
             resurrect_dir="$HOME/.local/share/tmux/resurrect"
             set -g @resurrect-dir $resurrect_dir
