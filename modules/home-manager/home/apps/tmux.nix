7bed0cd4de260ee95777214c789777dd2e4199d8
shemishtamesh <shemishtamail@gmail.com>
2024-11-10 20:33:19 +0200

    rebuilding home

--- modules/home-manager/home/apps/tmux.nix
+++ modules/home-manager/home/apps/tmux.nix
@@ -55,14 +55,14 @@ in
             set -g @resurrect-capture-pane-contents 'on'
 
             set -g @resurrect-strategy-nvim 'session'
-            # set -g @resurrect-processes '"~nvim->nvim"'
             resurrect_dir="$HOME/.local/share/tmux/resurrect"
             set -g @resurrect-dir $resurrect_dir
-            # set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g;s|/nix/store/.*nvim|nvim|g" $target | ${pkgs.moreutils}/bin/sponge $target'
             set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s|/nix/store/.*nvim|nvim|g" $target | ${pkgs.moreutils}/bin/sponge $target'
-            # set -g @resurrect-hook-post-save-all 'sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" ${resurrect_dir}/last | sponge ${resurrect_dir}/last'
           '';
 
+        # set -g @resurrect-processes '"~nvim->nvim"'
+        # set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g;s|/nix/store/.*nvim|nvim|g" $target | ${pkgs.moreutils}/bin/sponge $target'
+        # set -g @resurrect-hook-post-save-all 'sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g; s|/home/$USER/.nix-profile/bin/||g" ${resurrect_dir}/last | sponge ${resurrect_dir}/last'
       }
       {
         plugin = continuum;
