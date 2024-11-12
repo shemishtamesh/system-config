e9defa77f5b82b2962977ca51587079ec1e5c6b0
shemishtamesh <shemishtamail@gmail.com>
2024-11-12 20:34:55 +0200

    rebuilding nixos

--- flake.nix
+++ flake.nix
@@ -25,10 +25,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    stylix.url = "github:danth/stylix";
+    # stylix = {
+    #   url = "github:danth/stylix";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
   outputs =
