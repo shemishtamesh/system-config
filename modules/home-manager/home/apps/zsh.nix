c8e932a06b9b3bed78053f152c8c154d9e4081c5
shemishtamesh <shemishtamail@gmail.com>
2024-12-21 13:42:25 +0200

    rebuilding home

--- modules/home-manager/home/apps/zsh.nix
+++ modules/home-manager/home/apps/zsh.nix
@@ -38,7 +38,7 @@
   home.file.".zsh/completions/_cht" = {
     source = builtins.fetchurl {
       url = "https://cheat.sh/:zsh";
-      sha256 = "<calculated-sha256-hash>";
+      sha256 = "";
       name = "cht-sh-zsh-completion";
     };
   };
