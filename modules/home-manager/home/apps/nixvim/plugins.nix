c518204cd2152fa47bc29d901d2fad0c43b6c4f6
shemishtamesh <shemishtamail@gmail.com>
2024-12-21 14:44:09 +0200

    rebuilding home

--- modules/home-manager/home/apps/nixvim/plugins.nix
+++ modules/home-manager/home/apps/nixvim/plugins.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./plugins/lsp.nix
@@ -15,25 +17,36 @@
     # ./plugins/obsidian.nix
     # ./plugins/alpha.nix
   ];
-  programs.nixvim.plugins = {
-    lualine.enable = true;
-    nvim-colorizer.enable = true;
-    tmux-navigator.enable = true;
-    otter.enable = true;
-    numbertoggle.enable = true;
-    gitsigns.enable = true;
-    web-devicons.enable = true;
-    snacks.enable = true;
-    # rustaceanvim.enable = true;
-    # "dressing.nvim".enable = true;
-    # vimtex.enable = true;
-    # vim-surround.enable = true;
-    # gen.enable = true
-    # vim-asciidoctor.enable = true;
-    # beacon.enable = true;
-    # vim-dadbod.enable = true;
-    # vim-dadbod-ui.enable = true;
-    # vim-dadbod-completion.enable = true;
-    # noice.enable =  true;
+  programs.nixvim = {
+    plugins = {
+      lualine.enable = true;
+      nvim-colorizer.enable = true;
+      tmux-navigator.enable = true;
+      otter.enable = true;
+      numbertoggle.enable = true;
+      gitsigns.enable = true;
+      web-devicons.enable = true;
+      snacks.enable = true;
+      # rustaceanvim.enable = true;
+      # "dressing.nvim".enable = true;
+      # vimtex.enable = true;
+      # vim-surround.enable = true;
+      # gen.enable = true
+      # vim-asciidoctor.enable = true;
+      # beacon.enable = true;
+      # noice.enable =  true;
+    };
+    extraPlugins = with pkgs; [
+      (buildVimPluginFrom2Nix {
+        pname = "cheat.sh-vim";
+        version = "latest";
+        src = fetchFromGitHub {
+          owner = "dbeniamine";
+          repo = "cheat.sh-vim";
+          rev = "main";
+          sha256 = "<calculated-sha256>";
+        };
+      })
+    ];
   };
 }
