20ceb5a9c9271686acaf51ae654408172d0adc08
shemishtamesh <shemishtamail@gmail.com>
2024-12-24 15:28:13 +0200

    rebuilding home

--- modules/home-manager/home/apps/nixvim/plugins/telescope.nix
+++ modules/home-manager/home/apps/nixvim/plugins/telescope.nix
@@ -5,23 +5,23 @@ in
   programs.nixvim = {
     plugins.telescope = {
       enable = true;
-      keymaps = [
-        (keymap "n" "<leader>fo" "<cmd>Telescope oldfiles<CR>" { })
-        (keymap "n" "<leader>ff" "<cmd>Telescope find_files<CR>" { })
-        (keymap "n" "<leader>fl" "<cmd>Telescope live_grep<CR>" { })
-        (keymap "n" "<leader>fg" "<cmd>Telescope git_files<CR>" { })
-        (keymap "n" "<leader>fs" "<cmd>Telescope grep_string<CR>" { })
-        (keymap "n" "<leader>fc" "<cmd>Telescope command_history<CR>" { })
-        (keymap "n" "<leader>fq" "<cmd>Telescope quickfix<CR>" { })
-        (keymap "n" "<leader>fj" "<cmd>Telescope jumplist<CR>" { })
-        (keymap "n" "<leader>fk" "<cmd>Telescope keymaps<CR>" { })
-      ];
-      # luaConfig.post = # lua
-      #   ''
-      #     if #vim.fn.argv() == 0 then
-      #       vim.cmd("Telescope find_files")
-      #     end
-      #   '';
+      luaConfig.post = # lua
+        ''
+          if #vim.fn.argv() == 0 then
+            vim.cmd("Telescope find_files")
+          end
+        '';
     };
+    keymaps = [
+      (keymap "n" "<leader>fo" "<cmd>Telescope oldfiles<CR>" { })
+      (keymap "n" "<leader>ff" "<cmd>Telescope find_files<CR>" { })
+      (keymap "n" "<leader>fl" "<cmd>Telescope live_grep<CR>" { })
+      (keymap "n" "<leader>fg" "<cmd>Telescope git_files<CR>" { })
+      (keymap "n" "<leader>fs" "<cmd>Telescope grep_string<CR>" { })
+      (keymap "n" "<leader>fc" "<cmd>Telescope command_history<CR>" { })
+      (keymap "n" "<leader>fq" "<cmd>Telescope quickfix<CR>" { })
+      (keymap "n" "<leader>fj" "<cmd>Telescope jumplist<CR>" { })
+      (keymap "n" "<leader>fk" "<cmd>Telescope keymaps<CR>" { })
+    ];
   };
 }
