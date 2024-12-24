cce32c546eb6395838e49c883561467c52aaf1da
shemishtamesh <shemishtamail@gmail.com>
2024-12-24 17:02:43 +0200

    rebuilding home

--- modules/home-manager/home/apps/nixvim/plugins/telescope.nix
+++ modules/home-manager/home/apps/nixvim/plugins/telescope.nix
@@ -11,8 +11,8 @@ in
             callback = function()
               if (
                 vim.bo.filetype ~= ""
-                and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] ~= ""
-                and vim.fn.argv(0) ~= ""
+                or vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] ~= ""
+                or vim.fn.argv(0) ~= ""
               ) then
                 return
               else
