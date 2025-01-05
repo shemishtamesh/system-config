e3f00cae8b27d380a5a82b6b32335638a02d1447
shemishtamesh <shemishtamail@gmail.com>
2025-01-05 15:10:16 +0200

    rebuilding home

--- modules/home-manager/home/apps/waybar.nix
+++ modules/home-manager/home/apps/waybar.nix
@@ -217,9 +217,9 @@ in
           capslock = true;
           scrolllock = true;
           format = {
-            numlock = "#{icon}";
-            capslock = "󰪛{icon}";
-            scrolllock = "{icon}";
+            numlock = "# {icon} ";
+            capslock = "󰪛 {icon} ";
+            scrolllock = " {icon} ";
           };
           format-icons = {
             locked = "";
