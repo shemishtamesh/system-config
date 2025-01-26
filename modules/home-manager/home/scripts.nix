973c4ca9d03ab244441f8fb4752de9e15fb3aa6f
shemishtamesh <shemishtamail@gmail.com>
2025-01-26 10:24:07 +0200

    rebuilding home

--- modules/home-manager/home/scripts.nix
+++ modules/home-manager/home/scripts.nix
@@ -43,10 +43,10 @@
     (pkgs.writeShellScriptBin "bak" ''
       filename="$1"
       if [[ "$filename" =~ .bak$ ]]; then
-          mv "$filename" "''${filename%.bak}";
+          mv -i "$filename" "''${filename%.bak}";
           exit 0;
       else
-          mv "$filename" "$filename.bak";
+          mv -i "$filename" "$filename.bak";
           exit 0;
       fi
       echo "Error: $filename is not a valid file or directory.";
