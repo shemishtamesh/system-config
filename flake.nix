8b2885e88962635a350266669db50d666215e1f8
shemishtamesh <shemishtamail@gmail.com>
2025-02-12 21:52:43 +0200

    rebuilding nixos

--- flake.nix
+++ flake.nix
@@ -7,42 +7,51 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     hyprland = {
       type = "git";
       url = "https://github.com/hyprwm/Hyprland";
       submodules = true;
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     zen-browser = {
       url = "github:MarceColl/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     stylix.url = "github:danth/stylix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       home-manager,
       stylix,
       hyprland,
       nixvim,
+      treefmt-nix,
       ...
     }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       theme = (import ./modules/general/theming.nix { inherit pkgs; });
+      treefmtEval = treefmt-nix.lib.evalModule pkgs (
+        { ... }:
+        {
+          projectRootFile = "flake.nix";
+        }
+      );
     in
     {
+      formatter.${system} = treefmtEval.${system}.config.build.wrapper;
+      checks = {
+        formatting = treefmtEval.${system}.config.build.check self;
+      };
       nixosConfigurations.shenixtamesh = nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit inputs;
