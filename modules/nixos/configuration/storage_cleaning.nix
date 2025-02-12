82f112cf0c080096a98ec2f19c0af0acabb5dd88
shemishtamesh <shemishtamail@gmail.com>
2025-02-12 21:14:10 +0200

    rebuilding nixos

--- modules/nixos/configuration/storage_cleaning.nix
+++ modules/nixos/configuration/storage_cleaning.nix
@@ -2,12 +2,10 @@
   nix = {
     gc = {
       automatic = true;
-      dates = "weekly";
       options = "--delete-older-than 30d";
     };
     optimise = {
       automatic = true;
-      dates = "weekly";
     };
     settings.auto-optimise-store = true;
   };
