edd46a5b5682afffb354459efe4edba515c01894
shemishtamesh <shemishtamail@gmail.com>
2025-02-19 07:40:47 +0200

    home rebuild failed

--- modules/home-manager/home/packages.nix
+++ modules/home-manager/home/packages.nix
@@ -6,7 +6,49 @@
   ];
 
   home.packages = with pkgs; [
-    inputs.nixvim.packages.${pkgs.system}.default
+    (inputs.nixvim.packages.${system}.default.extend {
+      colorschemes.base16 = {
+        enable = true;
+
+        colorscheme =
+          let
+            colors = config.lib.stylix.colors.withHashtag;
+          in
+          {
+            base00 = colors.base00;
+            base01 = colors.base01;
+            base02 = colors.base02;
+            base03 = colors.base03;
+            base04 = colors.base04;
+            base05 = colors.base05;
+            base06 = colors.base06;
+            base07 = colors.base07;
+            base08 = colors.base08;
+            base09 = colors.base09;
+            base0A = colors.base0A;
+            base0B = colors.base0B;
+            base0C = colors.base0C;
+            base0D = colors.base0D;
+            base0E = colors.base0E;
+            base0F = colors.base0F;
+          };
+      };
+
+      highlight =
+        let
+          cfg = config.stylix.targets.nixvim;
+          transparent = {
+            bg = "none";
+            ctermbg = "none";
+          };
+        in
+        {
+          Normal = lib.mkIf cfg.transparent_bg.main transparent;
+          NonText = lib.mkIf cfg.transparent_bg.main transparent;
+          SignColumn = lib.mkIf cfg.transparent_bg.sign_column transparent;
+        };
+    })
+    # inputs.nixvim.packages.${pkgs.system}.default
     atool
     unrar
     unzip
