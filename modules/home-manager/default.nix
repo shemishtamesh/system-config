7701d0a0d3f785229aa2bd4631c1384171274cc8
shemishtamesh <shemishtamail@gmail.com>
2025-02-20 22:49:07 +0200

    home rebuild failed

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -38,8 +38,11 @@
     indicator = true;
   };
 
+  # allowing unfree packages
   nixpkgs.config = import ./nixpkgs.nix;
   xdg.configFile."nixpkgs/config.nix".source = ./nixpkgs.nix;
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
