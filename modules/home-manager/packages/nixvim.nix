e389d9d31fb2bbf81ced92cf087cbb08e51de5d8
shemishtamesh <shemishtamail@gmail.com>
2025-02-23 09:58:16 +0200

    home rebuild failed

--- modules/home-manager/packages/nixvim.nix
+++ modules/home-manager/packages/nixvim.nix
@@ -11,24 +11,25 @@
       colorschemes.base16 = {
         enable = true;
 
-        colorscheme = with config.lib.stylix.colors.withHashtag; {
-          inherit base00;
-          # base00 = "#0000ff";
-          inherit base01;
-          inherit base02;
-          inherit base03;
-          inherit base04;
-          inherit base05;
-          inherit base06;
-          inherit base07;
-          inherit base08;
-          inherit base09;
-          inherit base0A;
-          inherit base0B;
-          inherit base0C;
-          inherit base0D;
-          inherit base0E;
-          inherit base0F;
+        # colorscheme = with config.lib.stylix.colors.withHashtag; {
+        colorscheme = {
+          # inherit base00;
+          base00 = "#0000ff";
+          base01 = config.lib.stylix.colors.withHashtag.base01;
+          base02 = config.lib.stylix.colors.withHashtag.base02;
+          base03 = config.lib.stylix.colors.withHashtag.base03;
+          base04 = config.lib.stylix.colors.withHashtag.base04;
+          base05 = config.lib.stylix.colors.withHashtag.base05;
+          base06 = config.lib.stylix.colors.withHashtag.base06;
+          base07 = config.lib.stylix.colors.withHashtag.base07;
+          base08 = config.lib.stylix.colors.withHashtag.base08;
+          base09 = config.lib.stylix.colors.withHashtag.base09;
+          base0A = config.lib.stylix.colors.withHashtag.base0A;
+          base0B = config.lib.stylix.colors.withHashtag.base0B;
+          base0C = config.lib.stylix.colors.withHashtag.base0C;
+          base0D = config.lib.stylix.colors.withHashtag.base0D;
+          base0E = config.lib.stylix.colors.withHashtag.base0E;
+          base0F = config.lib.stylix.colors.withHashtag.base0F;
         };
         settings.telescope_borders = true;
       };
