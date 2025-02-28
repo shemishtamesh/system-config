9aa31f9222ec9a29ef4f63f38877c1a190079093
shemishtamesh <shemishtamail@gmail.com>
2025-02-28 14:55:18 +0200

    format

--- modules/home-manager/packages/hyprland.nix
+++ modules/home-manager/packages/hyprland.nix
@@ -190,17 +190,16 @@ in
           "wind, 0.05, 0.9, 0.1, 1.05"
           "winIn, 0.1, 1.1, 0.1, 1.1"
           "winOut, 0.3, -0.3, 0, 1"
-          "liner, 1, 1, 1, 1"
+          "linear, 1, 1, 1, 1"
         ];
         animation = [
           "windows, 1, 6, wind, slide"
           "windowsIn, 1, 6, winIn, slide"
           "windowsOut, 1, 6, winOut, slide"
           "windowsMove, 1, 6, wind, slide"
-          "border, 1, 1, liner"
-          "borderangle, 1, 30, liner, loop"
-          "fade, 1, 10, default"
-          "fadeShadow, 1, 100, liner"
+          "border, 1, 1, linear"
+          "borderangle, 1, 30, linear, loop"
+          "fade, 1, 10, linear"
           "workspaces, 1, 5, wind"
         ];
       };
