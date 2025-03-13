ea17f836deaddc6d65f4413c399970e8180e973b
shemishtamesh <shemishtamail@gmail.com>
2025-03-13 16:55:56 +0200

    home rebuild failed

--- modules/shared/theme.nix
+++ modules/shared/theme.nix
@@ -84,7 +84,7 @@ in
             <rect
                width="${toString width}"
                height="${toString height}"
-               fill="#${palette.base00}"
+               fill="#${base00}"
                id="rect1" />
             <svg
                x="${toString (width / 2 - (logoScale * 50))}"
@@ -99,32 +99,32 @@ in
                    id="g2">
                   <path
                      d="m -249.0175,116.584 122.2,211.68 -56.157,0.5268 -32.624,-56.869 -32.856,56.565 -27.902,-0.011 -14.291,-24.69 46.81,-80.49 -33.229,-57.826 z"
-                     fill="#${palette.base08}"
+                     fill="#${base08}"
                      style="display:inline;isolation:auto;mix-blend-mode:normal"
                      id="path1" />
                   <path
                      d="m -204.9102,29.388 -122.22,211.67 -28.535,-48.37 32.938,-56.688 -65.415,-0.1717 -13.942,-24.169 14.237,-24.721 93.111,0.2937 33.464,-57.69 z"
-                     fill="#${palette.base09}"
+                     fill="#${base09}"
                      id="path2"
                      style="display:inline" />
                   <path
                      d="m -195.535,198.588 244.42,0.012 -27.622,48.897 -65.562,-0.1813 32.559,56.737 -13.961,24.158 -28.528,0.031 -46.301,-80.784 -66.693,-0.1359 z"
-                     fill="#${palette.base0A}"
+                     fill="#${base0A}"
                      id="path3"
                      style="display:inline" />
                   <path
                      d="m -53.275,105.84 -122.2,-211.68 56.157,-0.5268 32.624,56.869 32.856,-56.565 27.902,0.011 14.291,24.69 -46.81,80.49 33.229,57.826 z"
-                     fill="#${palette.base0B}"
+                     fill="#${base0B}"
                      id="path4"
                      style="display:inline" />
                   <path
                      d="m -97.659,193.01 122.22,-211.67 28.535,48.37 -32.938,56.688 65.415,0.1716 13.941,24.169 -14.237,24.721 -93.111,-0.2937 -33.464,57.69 z"
-                     fill="#${palette.base0C}"
+                     fill="#${base0C}"
                      style="display:inline;isolation:auto;mix-blend-mode:normal"
                      id="path5" />
                   <path
                      d="m -107.2575,23.36 -244.42,-0.012 27.622,-48.897 65.562,0.1813 -32.559,-56.737 13.961,-24.158 28.528,-0.031 46.301,80.784 66.693,0.1359 z"
-                     fill="#${palette.base0D}"
+                     fill="#${base0D}"
                      style="display:inline;isolation:auto;mix-blend-mode:normal"
                      id="path6" />
                 </g>
