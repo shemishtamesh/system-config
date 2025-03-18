2ba766bb828524d6de8bd61d3c0845beed221977
shemishtamesh <shemishtamail@gmail.com>
2025-03-18 14:12:58 +0200

    format

--- modules/profiles/users/shemishtamesh/home/packages/hyprpaper.nix
+++ modules/profiles/users/shemishtamesh/home/packages/hyprpaper.nix
@@ -3,20 +3,29 @@
   host,
   ...
 }:
+let
+  wallpapers = builtins.attrValues (
+    builtins.mapAttrs (
+      portname:
+      {
+        width,
+        height,
+        ...
+      }:
+      {
+        path = toString (shared.theme.wallpaper { inherit portname width height; });
+        inherit portname;
+      }
+    ) host.monitors
+  );
+in
 {
   stylix.targets.hyprpaper.enable = false;
   services.hyprpaper = {
     enable = true;
-    settings.wallpaper = builtins.attrValues (
-      builtins.mapAttrs (
-        portname:
-        {
-          width,
-          height,
-          ...
-        }:
-        "${portname}, ${toString (shared.theme.wallpaper { inherit portname width height; })}"
-      ) host.monitors
-    );
+    settings = {
+      load = map (wallpaper: wallpaper.path) wallpapers;
+      wallpaper = map (wallpaper: "${wallpaper.portname}, ${wallpaper.path}") wallpapers;
+    };
   };
 }
