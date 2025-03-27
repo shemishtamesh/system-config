88d10e22cffb5ca1a52dba0fa2773affcdf9ef6f
shemishtamesh <shemishtamail@gmail.com>
2025-03-27 20:39:16 +0200

    before formatting

--- modules/users/shemishtamesh/packages/hyprland/default.nix
+++ modules/users/shemishtamesh/packages/hyprland/default.nix
@@ -227,8 +227,8 @@ in
         ];
         workspace = [
           # no borders when there's only a single visible window
-          "w[v1], gapsout:0, gapsin:0"
-          "f[1], gapsout:0, gapsin:0"
+          "s[false] w[v1], gapsout:0, gapsin:0"
+          "s[false] f[1], gapsout:0, gapsin:0"
         ];
         exec-once = [
           "${pkgs.hyprpaper}"
