c1c2ee59a60c4f572dff8b94d988e5f26bc7d365
shemishtamesh <shemishtamail@gmail.com>
2025-04-25 09:35:20 +0300

    before formatting

--- modules/hosts/shenixtamesh/configuration/default.nix
+++ modules/hosts/shenixtamesh/configuration/default.nix
@@ -104,8 +104,14 @@
     ];
 
     # https://github.com/cachix/cachix/issues/259
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     trusted-users = [ "@wheel" ];
   };
 
