bd0b3b3f0bd7de9defdcd2d3f425480cf5691c8d
shemishtamesh <shemishtamail@gmail.com>
2025-04-26 12:19:09 +0300

    before formatting

--- modules/users/shemishtamesh/packages/btop.nix
+++ modules/users/shemishtamesh/packages/btop.nix
@@ -8,6 +8,22 @@
       shown_boxes = "proc cpu mem net gpu0";
       proc_sorting = "cpu direct";
       update_ms = 100;
+      proc_cpu_graphs = true;
+      proc_filter_kernel = false;
+      proc_aggregate = true;
+      cpu_graph_upper = "user";
+      cpu_graph_lower = "system";
+      show_gpu_info = "Auto";
+      clock_format = "%X";
+      disks_filter = "";
+      use_fstab = true;
+      zfs_hide_datasets = false;
+      io_mode = true;
+      net_sync = true;
+      net_iface = "";
+      show_battery_watts = true;
+      nvml_measure_pcie_speeds = true;
+      gpu_mirror_graph = false;
     };
   };
 }
