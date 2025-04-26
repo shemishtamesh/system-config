8cb035937681427b2207ef31ca7459ac641f6e31
shemishtamesh <shemishtamail@gmail.com>
2025-04-26 12:14:51 +0300

    system switch succeeded

--- modules/users/shemishtamesh/packages/btop.nix
+++ modules/users/shemishtamesh/packages/btop.nix
@@ -1,8 +1,13 @@
 {
   programs.btop = {
     enable = true;
-    # settings = {
-    #
-    # };
+    settings = {
+      presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
+      vim_keys = true;
+      graph_symbol_gpu = "default";
+      shown_boxes = "proc cpu mem net gpu0";
+      proc_sorting = "cpu direct";
+      update_ms = 100;
+    };
   };
 }
