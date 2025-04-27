e89856bba45a8901f98b833b4e6ccc167d87b5c8
shemishtamesh <shemishtamail@gmail.com>
2025-04-27 23:43:38 +0300

    home config switch failed

--- modules/shared/theme.nix
+++ modules/shared/theme.nix
@@ -80,7 +80,7 @@ in
         ''
           colors = ["#${base00}", "#${base01}", "#${base02}", "#${base03}", "#${base04}", "#${base05}", "#${base06}", "#${base07}", "#${base08}", "#${base09}", "#${base0A}", "#${base0B}", "#${base0C}", "#${base0D}", "#${base0E}", "#${base0F}", "#${base10}", "#${base11}", "#${base12}", "#${base13}", "#${base14}", "#${base15}", "#${base16}", "#${base17}"];
 
-          $image_height = ${height};
+          $image_height = ${toString height};
           $number_of_triangles_on_y_axis = 70;
           $triangle_height = $image_height / $number_of_triangles_on_y_axis;
           triangle_side = $triangle_height / sin(60);
