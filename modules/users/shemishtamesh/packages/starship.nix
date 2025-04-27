b42ea8e03c788f3a909fd3873c0969d2a0c1f3e2
shemishtamesh <shemishtamail@gmail.com>
2025-04-27 09:56:21 +0300

    before formatting

--- modules/users/shemishtamesh/packages/starship.nix
+++ modules/users/shemishtamesh/packages/starship.nix
@@ -1,1 +1,8 @@
-{ programs.starship.enable = true; }
+{
+  programs.starship = {
+    enable = true;
+    settings = {
+      nix_shell.huristics = true; # https://github.com/starship/starship/pull/4724#pullrequestreview-1222025226
+    };
+  };
+}
