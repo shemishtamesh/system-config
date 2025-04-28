f983065fd9cb0fbce0218b0e187293c278cfaa9b
shemishtamesh <shemishtamail@gmail.com>
2025-04-28 16:43:19 +0300

    before formatting

--- modules/shared/theme.nix
+++ modules/shared/theme.nix
@@ -110,7 +110,7 @@ in
             GAPS = TRIANGLE_HEIGHT * 4 / 25
             EDGE_BUFFER = 1
             RADIUS_RATIO = 2.6
-            DISTANCE_FADE_SCALE = 0.5
+            DISTANCE_FADE_SCALE = 0.3
             COLORS = [
                 "#${base00}",
                 "#${base01}",
