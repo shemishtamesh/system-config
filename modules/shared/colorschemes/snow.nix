e8021f3fbef7a48450a85a6b83f810b0b92cbd40
Yuval Inov <yuval@agamanalytics.com>
2025-04-29 17:27:17 +0300

    before formatting

---
+++ modules/shared/colorschemes/snow.nix
@@ -0,0 +1,35 @@
+{
+  system = "base24";
+  name = "custom";
+  slug = "custom";
+  author = "";
+  variant = "dark";
+  palette = {
+    base00 = "000000"; # #000000
+    base01 = "0b1520"; # #0b1520
+    base02 = "1a2e42"; # #1a2e42
+    base03 = "27435f"; # #27435f
+    base04 = "62819e"; # #62819e
+    base05 = "92abc5"; # #92abc5
+    base06 = "b9d3e8"; # #b9d3e8
+    base07 = "e3f0f8"; # #e3f0f8
+
+    base08 = "ff6b6b"; # #ff6b6b
+    base09 = "ffab6b"; # #ffab6b
+    base0A = "f0fa90"; # #f0fa90
+    base0B = "6af09e"; # #6af09e
+    base0C = "8f8fff"; # #8f8fff
+    base0D = "569dff"; # #569dff
+    base0E = "c3a1ff"; # #c3a1ff
+    base0F = "b48f6a"; # #b48f6a
+
+    base10 = "ff8f8f"; # #ff8f8f
+    base11 = "ffc28f"; # #ffc28f
+    base12 = "ffffaf"; # #ffffaf
+    base13 = "8dffb9"; # #8dffb9
+    base14 = "afafff"; # #afafff
+    base15 = "a3cfff"; # #a3cfff
+    base16 = "e5cfff"; # #e5cfff
+    base17 = "d7b18f"; # #d7b18f
+  };
+}
