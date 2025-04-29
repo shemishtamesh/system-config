d9ba2c52a3d7e657e75a7dd5ff97ae66be8ba618
Yuval Inov <yuval@agamanalytics.com>
2025-04-29 17:37:09 +0300

    before formatting

--- modules/hosts/shemishtamac/configuration/services/default.nix
+++ modules/hosts/shemishtamac/configuration/services/default.nix
@@ -3,6 +3,6 @@
     ./yabai.nix
     ./skhd.nix
     ./sketchybar.nix
-    ./docker.nix # not really a service, functions as one
+    ./docker.nix # not really a service, should function in a similar way
   ];
 }
--- modules/hosts/shemishtamac/configuration/services/docker.nix
+++ modules/hosts/shemishtamac/configuration/services/docker.nix
@@ -1,19 +1,17 @@
 # taken from here https://github.com/nix-darwin/nix-darwin/issues/1182#issuecomment-2485401568
-{ pkgs, ... }:
 {
-  launchd.agents."colima.default" = {
-    command = "${pkgs.colima}/bin/colima start --foreground";
-    serviceConfig = {
-      Label = "com.colima.default";
-      RunAtLoad = true;
-      KeepAlive = true;
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [
+    docker
+  ];
 
-      StandardOutPath = "/var/log/colima/default/daemon/launchd.stdout.log";
-      StandardErrorPath = "/var/log/colima/default/daemon/launchd.stderr.log";
-
-      EnvironmentVariables = {
-        PATH = "${pkgs.colima}/bin:${pkgs.docker}/bin:/usr/bin:/bin:/usr/sbin:/sbin";
-      };
-    };
+  services.colima = {
+    enable = true;
+    createDockerSocket = true;
+    groupMembers = [
+      "my-username"
+    ];
   };
 }
