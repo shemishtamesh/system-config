18497c4f1cd42c13fdbc239f689b32183558bb35
shemishtamesh <shemishtamail@gmail.com>
2025-04-30 17:05:12 +0300

    home config switch failed

--- modules/users/shemishtamesh/packages/nixvim.nix
+++ modules/users/shemishtamesh/packages/nixvim.nix
@@ -7,48 +7,50 @@
 
 {
   home.packages = with pkgs; [
-    (inputs.nixvim.wrappedNvim (inputs.nixvim.packages.${system}.nvim.extend {
-      colorschemes.base16 = {
-        enable = true;
+    (inputs.nixvim.wrappedNvim (
+      inputs.nixvim.packages.${system}.nvim.extend {
+        colorschemes.base16 = {
+          enable = true;
 
-        colorscheme =
-          with config.lib.stylix.colors.withHashtag;
-          lib.mkForce {
-            inherit
-              base00
-              base01
-              base02
-              base03
-              base04
-              base05
-              base06
-              base07
-              base08
-              base09
-              base0A
-              base0B
-              base0C
-              base0D
-              base0E
-              base0F
-              ;
-          };
-        settings.telescope_borders = true;
-      };
-
-      highlight =
-        let
-          cfg = config.stylix.targets.nixvim;
-          transparent = {
-            bg = "none";
-            ctermbg = "none";
-          };
-        in
-        {
-          Normal = lib.mkIf cfg.transparentBackground.main transparent;
-          NonText = lib.mkIf cfg.transparentBackground.main transparent;
-          SignColumn = lib.mkIf cfg.transparentBackground.signColumn transparent;
+          colorscheme =
+            with config.lib.stylix.colors.withHashtag;
+            lib.mkForce {
+              inherit
+                base00
+                base01
+                base02
+                base03
+                base04
+                base05
+                base06
+                base07
+                base08
+                base09
+                base0A
+                base0B
+                base0C
+                base0D
+                base0E
+                base0F
+                ;
+            };
+          settings.telescope_borders = true;
         };
-    }))
+
+        highlight =
+          let
+            cfg = config.stylix.targets.nixvim;
+            transparent = {
+              bg = "none";
+              ctermbg = "none";
+            };
+          in
+          {
+            Normal = lib.mkIf cfg.transparentBackground.main transparent;
+            NonText = lib.mkIf cfg.transparentBackground.main transparent;
+            SignColumn = lib.mkIf cfg.transparentBackground.signColumn transparent;
+          };
+      }
+    ))
   ];
 }
