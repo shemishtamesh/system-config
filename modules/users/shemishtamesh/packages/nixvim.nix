5f9d9faa18a952fd36245143fe585b2eb9f71b87
shemishtamesh <shemishtamail@gmail.com>
2025-04-30 17:34:29 +0300

    home config switch failed

--- modules/users/shemishtamesh/packages/nixvim.nix
+++ modules/users/shemishtamesh/packages/nixvim.nix
@@ -8,7 +8,7 @@
 {
   home.packages = with pkgs; [
     (inputs.nixvim.wrappedNvim stdenv.system (
-      inputs.nixvim.packages.${system}.nvim.extend {
+      inputs.nixvim.packages.${stdenv.system}.nvim.extend {
         colorschemes.base16 = {
           enable = true;
 
