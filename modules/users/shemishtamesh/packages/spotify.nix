8127444d8c653dc4f518c57ea49114b7e03a793d
shemishtamesh <shemishtamail@gmail.com>
2025-04-30 10:38:55 +0300

    before formatting

--- modules/users/shemishtamesh/packages/spotify.nix
+++ modules/users/shemishtamesh/packages/spotify.nix
@@ -3,7 +3,7 @@ let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in
 {
-  imports = [ inputs.spicetify-nix.nixosModules.spicetify ];
+  imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
   programs.spicetify = {
     enable = true;
     enabledExtensions = with spicePkgs.extensions; [
