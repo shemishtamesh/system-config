d3f0f9bc8dea6501f159797a1cdc6d17eec57a74
shemishtamesh <shemishtamail@gmail.com>
2025-04-30 10:36:29 +0300

    home config switch failed

--- modules/users/shemishtamesh/packages/spotify.nix
+++ modules/users/shemishtamesh/packages/spotify.nix
@@ -3,6 +3,7 @@ let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
 in
 {
+  imports = [ inputs.spicetify-nix.nixosModules.spicetify ];
   programs.spicetify = {
     enable = true;
     enabledExtensions = with spicePkgs.extensions; [
