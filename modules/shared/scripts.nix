78fd18ed53a20ca5cfdea01175302068d196ffa6
shemishtamesh <shemishtamail@gmail.com>
2025-05-01 10:21:24 +0300

    before formatting

--- modules/shared/scripts.nix
+++ modules/shared/scripts.nix
@@ -1,14 +1,22 @@
-pkgs: {
-  sync_external_monitors_brightness =
-    let
-      ddcutil = "${pkgs.ddcutil}/bin/ddcutil";
-      brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
-      bc = "${pkgs.bc}/bin/bc";
-    in
-    # sh
-    pkgs.lib.getExe (
-      pkgs.writeShellScriptBin "sync_external_monitors_brightness" ''
-        ${ddcutil} setvcp 10 $(echo \"$(echo \"$(${brightnessctl} g) / $(${brightnessctl} m) * 100\" | ${bc} -l | ${bc}) / 1\" | ${bc})
-      ''
-    );
+pkgs:
+let
+  ddcutil = "${pkgs.ddcutil}/bin/ddcutil";
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  bc = "${pkgs.bc}/bin/bc";
+in
+{
+  sync_external_monitors_brightness = pkgs.lib.getExe (
+    pkgs.writeShellScriptBin "sync_external_monitors_brightness" ''
+      ${ddcutil} setvcp 10 $(echo \"$(echo \"$(${brightnessctl} g) / $(${brightnessctl} m) * 100\" | ${bc} -l | ${bc}) / 1\" | ${bc})
+    ''
+  );
+  set_brightness = pkgs.lib.getExe (
+    pkgs.writeShellScriptBin "set_brightness" ''
+      for bus in $(${ddcutil} detect --terse \
+                        | grep -F "I2C bus:" \
+                        | awk -F '-' '{print $2}'); do
+          ${ddcutil} setvcp 10 $1 --bus "$bus"
+      done
+    ''
+  );
 }
