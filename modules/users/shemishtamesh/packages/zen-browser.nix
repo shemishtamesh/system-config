2c73ce68d6e1a98967694fb3691d7fbcf52bf4da
Yuval Inov <yuval@agamanalytics.com>
2025-05-02 09:35:09 +0300

    home config switch failed

--- modules/users/shemishtamesh/packages/zen-browser.nix
+++ modules/users/shemishtamesh/packages/zen-browser.nix
@@ -1,20 +1,34 @@
-{ pkgs, inputs, ... }:
 {
-  imports = [ inputs.zen-browser.homeModules.twilight ];
-  programs.zen-browser = {
-    enable = true;
-    policies = {
-      AutofillAddressEnabled = true;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
+  pkgs,
+  inputs,
+  host,
+  ...
+}:
+if (host.system == "x86_64-linux") || (host.system == "aarch64-linux") then
+  {
+    imports = [ inputs.zen-browser.homeModules.twilight ];
+    programs.zen-browser = {
+      enable = true;
+      policies = {
+        AutofillAddressEnabled = true;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+      };
+      nativeMessagingHosts = [ pkgs.firefoxpwa ];
     };
-    nativeMessagingHosts = [ pkgs.firefoxpwa ];
-  };
-}
+  }
+else
+  {
+    home.programs = [
+      (inputs.zen-browser.packages."${host.system}".default.override {
+        nativeMessagingHosts = [ pkgs.firefoxpwa ];
+      })
+    ];
+  }
