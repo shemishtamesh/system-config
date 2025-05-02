c529605d91f498f09b7e18c13575a59fdb3aa358
Yuval Inov <yuval@agamanalytics.com>
2025-05-02 12:25:22 +0300

    before formatting

--- modules/users/shemishtamesh/packages/starship.nix
+++ modules/users/shemishtamesh/packages/starship.nix
@@ -3,6 +3,7 @@
     enable = true;
     settings = {
       nix_shell.heuristic = true; # https://github.com/starship/starship/pull/4724#pullrequestreview-1222025226
+      shlvl.disabled = false;
     };
   };
 }
