344fe9d784d3e28d5b13aae465fc289c2bff3d48
Yuval Inov <yuval@agamanalytics.com>
2025-05-02 17:11:11 +0300

    os config switch failed

--- modules/hosts/shemishtamac/configuration/services/docker.nix
+++ modules/hosts/shemishtamac/configuration/services/docker.nix
@@ -5,16 +5,25 @@ with pkgs;
     colima
     docker
   ];
-
-  launchd.user.agents.colima = {
+  launchd.agents."colima.default" = {
     path = [
       colima
       docker
     ];
     command = "${colima}/bin/colima start --foreground";
     serviceConfig = {
-      KeepAlive = true;
+      Label = "com.colima.default";
       RunAtLoad = true;
+      KeepAlive = true;
+
+      # not sure where to put these paths and not reference a hard-coded `$HOME`; `/var/log`?
+      StandardOutPath = "/var/log/.colima/default/daemon/launchd.stdout.log";
+      StandardErrorPath = "/var/log/.colima/default/daemon/launchd.stderr.log";
+
+      # not using launchd.agents.<name>.path because colima needs the system ones as well
+      EnvironmentVariables = {
+        PATH = "${colima}/bin:${docker}/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+      };
     };
   };
 }
