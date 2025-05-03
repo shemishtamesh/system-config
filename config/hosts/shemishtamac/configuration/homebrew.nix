0928aeba97d2023cb8ef18afe005402bf05dce5b
Yuval Inov <yuval@agamanalytics.com>
2025-05-03 10:46:02 +0300

    system switch succeeded

--- config/hosts/shemishtamac/configuration/homebrew.nix
+++ config/hosts/shemishtamac/configuration/homebrew.nix
@@ -1,21 +1,17 @@
 { inputs, host, ... }:
 {
+  imports = [ inputs.nix-homebrew.darwinModules.nix-homebrew ];
+  nix-homebrew = {
+    enable = true;
+    user = builtins.elemAt (builtins.attrNames host.users) 0;
+    taps = {
+      "homebrew/homebrew-core" = inputs.homebrew-core;
+      "homebrew/homebrew-cask" = inputs.homebrew-cask;
+    };
+    mutableTaps = false;
+  };
   homebrew = {
     enable = true;
-    brews = [ "zen-browser" ];
+    brews = [ "watch" ];
   };
-  imports = [
-    inputs.nix-homebrew.darwinModules.nix-homebrew
-    {
-      nix-homebrew = {
-        enable = true;
-        user = builtins.elemAt (builtins.attrNames host.users) 0;
-        taps = {
-          "homebrew/homebrew-core" = inputs.homebrew-core;
-          "homebrew/homebrew-cask" = inputs.homebrew-cask;
-        };
-        mutableTaps = false;
-      };
-    }
-  ];
 }
