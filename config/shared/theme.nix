66aeee20b335771d50fab4f7a4d362b3c5aaa307
shemishtamesh <shemishtamail@gmail.com>
2025-05-04 22:14:25 +0300

    before formatting

--- config/shared/theme.nix
+++ config/shared/theme.nix
@@ -65,9 +65,9 @@ in
           ${if !background then "--no_background" else ""} \
           ${if !palette then "--no_palette" else ""} \
           ${if !nix then "--no_nix" else ""} \
-          ${if !gaps then "--gaps 0" else "--gaps 300"} \
+          ${if !gaps then "--gaps 0" else "--gaps 200"} \
           ${if !random then "--no_random" else ""} \
-          --no_color_outside_nix
+          # --no_color_outside_nix
       '';
       installPhase = "install -Dm0644 ${name} $out";
     };
