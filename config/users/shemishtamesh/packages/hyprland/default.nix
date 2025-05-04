1e32f453826723618f134d87a7fc0095e2010cf5
shemishtamesh <shemishtamail@gmail.com>
2025-05-04 14:57:59 +0300

    before formatting

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -182,10 +182,10 @@ in
 
           resize_on_border = true;
 
-          "col.inactive_border" = lib.mkForce "0x00000000";
+          "col.inactive_border" = lib.mkForce "0x00000000"; # transparent
           "col.active_border" =
             with config.lib.stylix.colors;
-            lib.mkForce "rgb(${base08}) rgb(${base09}) rgb(${base0A}) rgb(${base0B}) rgb(${base0C}) rgb(${base0D}) rgb(${base0E}) rgb(${base0F})";
+            lib.mkForce "rgb(${base08}) rgb(${base0A}) rgb(${base0B}) rgb(${base0C}) rgb(${base0D}) rgb(${base0E})";
         };
         cursor = {
           hide_on_key_press = true;
