31bc4a20660efac517244173de0692c4692a89a5
shemishtamesh <shemishtamail@gmail.com>
2025-05-04 11:25:29 +0300

    before formatting

--- config/shared/wallpaper_generator.py
+++ config/shared/wallpaper_generator.py
@@ -208,7 +208,7 @@ class Background:
             print('got here' * 10)
             colors = colors[18:16:-1] + colors[:16] + colors[18:]
         print('#' * 100)
-        print(list(enumerate([str(color) for color in colors[16:18:-1]])))
+        print(list(enumerate([str(color) for color in colors[18:16:-1]])))
         print(str(colors[16]))
         print(str(colors[17]))
         print('#' * 100)
