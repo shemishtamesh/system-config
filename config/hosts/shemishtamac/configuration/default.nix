a445f8afe5d788b749329d2cdcd8361d7088c209
Yuval Inov <yuval@agamanalytics.com>
2025-05-05 16:15:19 +0300

    os config switch failed

--- config/hosts/shemishtamac/configuration/default.nix
+++ config/hosts/shemishtamac/configuration/default.nix
@@ -59,7 +59,10 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    trusted-users = [ "@wheel" ];
+    trusted-users = [
+      builtins.attrNames
+      host.users
+    ];
   };
 
   # use touch id auth for sudo
