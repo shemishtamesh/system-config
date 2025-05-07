448b475f6f2cb82f9c4a7fee92acae3d6f6932ea
Yuval Inov <yuval@agamanalytics.com>
2025-05-07 12:36:46 +0300

    home config switch failed

--- config/users/shemishtamesh/packages/git.nix
+++ config/users/shemishtamesh/packages/git.nix
@@ -40,7 +40,7 @@
     hooks.pre-commit =
       pkgs.writeScript "pre-commit-script" # sh
         ''
-          ${pkgs.gitleaks}/bin/gitleaks git --pre-commit --verbose --redact
+          ${pkgs.gitleaks}/bin/gitleaks git --no-banner --pre-commit --verbose --redact
         '';
     extraConfig = {
       merge = {
