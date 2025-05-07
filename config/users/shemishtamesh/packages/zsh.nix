56bf277901d51ccda8c7f83821a0d35d4dbaa300
Yuval Inov <yuval@agamanalytics.com>
2025-05-07 11:09:58 +0300

    before formatting

--- config/users/shemishtamesh/packages/zsh.nix
+++ config/users/shemishtamesh/packages/zsh.nix
@@ -84,6 +84,7 @@
         # Edit line in vim with ctrl-e:
         autoload edit-command-line; zle -N edit-command-line
         bindkey '^e' edit-command-line
+
         ${
           if pkgs.lib.last (pkgs.lib.splitString "-" host.system) == "darwin" then
             ''
