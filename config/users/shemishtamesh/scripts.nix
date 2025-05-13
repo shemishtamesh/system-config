80aa63978c0cab95ffc996aac72abe1a1e21fd71
shemishtamesh <shemishtamail@gmail.com>
2025-05-13 21:21:07 +0300

    before formatting

--- config/users/shemishtamesh/scripts.nix
+++ config/users/shemishtamesh/scripts.nix
@@ -15,14 +15,16 @@
     '')
     (pkgs.writeShellScriptBin "s" ''
       if [[ $# -eq 0 ]]; then
-        echo 'Enters a nix shell with the specified packages, and starting $SHELL. Usage: s <arg1> <arg2> ...'
+        echo 'Enters a nix shell with the specified packages, and starting $SHELL.'
+        echo 'Usage: s <arg1> <arg2> ...'
         exit 1
       fi
       command="nix --extra-experimental-features nix-command --extra-experimental-features flakes shell"
       for arg in "$@"; do
         command="$command nixpkgs#$arg"
       done
-      eval "$command --command $SHELL"
+      export NIXPKGS_ALLOW_UNFREE=1
+      eval "$command --impure --command $SHELL"
     '')
   ];
 }
