84c5475e03597b61eb34541e320197e300022047
shemishtamesh <shemishtamail@gmail.com>
2025-05-13 13:21:14 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/starship.nix
+++ config/users/shemishtamesh/packages/starship.nix
@@ -3,7 +3,10 @@
     enable = true;
     settings = {
       nix_shell.heuristic = true; # https://github.com/starship/starship/pull/4724#pullrequestreview-1222025226
-      shlvl.disabled = false;
+      shlvl = {
+        disabled = false;
+        threshold = 3;
+      };
     };
   };
 }
