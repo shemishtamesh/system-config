5c816e30dbaece541a79901e987f40e58f081738
Yuval Inov <yuval@agamanalytics.com>
2025-05-14 09:45:13 +0300

    before formatting

--- config/hosts/shemishtamac/configuration/services/skhd.nix
+++ config/hosts/shemishtamac/configuration/services/skhd.nix
@@ -46,7 +46,7 @@
       fn - b : yabai -m space --balance
 
       # launch apps
-      fn - return : kitty --execute tmux new-session -c $HOME
+      fn - "return" : kitty --execute tmux new-session -c $HOME
     '';
   };
 }
