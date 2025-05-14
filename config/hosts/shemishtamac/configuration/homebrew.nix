5420dbf3ce2924ff577cdfc43b3ff47b1ad66d48
Yuval Inov <yuval@agamanalytics.com>
2025-05-14 10:06:48 +0300

    before formatting

--- config/hosts/shemishtamac/configuration/homebrew.nix
+++ config/hosts/shemishtamac/configuration/homebrew.nix
@@ -7,12 +7,10 @@
   };
   homebrew = {
     enable = true;
-    # casks = [ "zen-browser" ];
-    brews = [ "zen-browser" ];
+    casks = [ "zen-browser" ];
     onActivation = {
       # cleanup = "uninstall"; # WARNING: would be preferable, but seems to cause brew to uninstall everything (even if listed here), and then install it in the next rebuild
       autoUpdate = true;
     };
-    taps = [ "homebrew/cask" ];
   };
 }
