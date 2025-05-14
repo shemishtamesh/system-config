5928813e6797f720fcdd4c2d3a57ab8b5cb66d64
Yuval Inov <yuval@agamanalytics.com>
2025-05-14 10:01:10 +0300

    os config switch failed

--- config/hosts/shemishtamac/configuration/homebrew.nix
+++ config/hosts/shemishtamac/configuration/homebrew.nix
@@ -8,7 +8,7 @@
   homebrew = {
     enable = true;
     # casks = [ "zen-browser" ];
-    brew = [ "zen-browser" ];
+    brews = [ "zen-browser" ];
     onActivation = {
       # cleanup = "uninstall"; # WARNING: would be preferable, but seems to cause brew to uninstall everything (even if listed here), and then install it in the next rebuild
       autoUpdate = true;
