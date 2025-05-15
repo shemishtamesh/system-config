f235cafb5d21c1cf8ea0cc9f1be00f894da9a8c1
Yuval Inov <yuval@agamanalytics.com>
2025-05-15 17:46:00 +0300

    home config switch failed

--- config/users/shemishtamesh/environment_variables.nix
+++ config/users/shemishtamesh/environment_variables.nix
@@ -1,8 +1,12 @@
+{ host, lib, ... }:
+let
+  system_type = lib.last (lib.splitString "-" host.system);
+in
 {
   home.sessionVariables = {
     MANROFFOPT = "-c";
     MANWIDTH = "999";
     MANPAGER = "nvim +Man!";
     EDITOR = "nvim";
-  };
+  } // (if system_type == "darwin" then host.system { HOMEBREW_NO_ANALYTICS = 1; } else { });
 }
