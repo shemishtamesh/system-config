9486827930ecb62ed9e42541d2c86bd91cd3de9d
shemishtamesh <shemishtamail@gmail.com>
2025-05-17 14:44:28 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/nixcord.nix
+++ config/users/shemishtamesh/packages/nixcord.nix
@@ -1,46 +1,49 @@
 { inputs, pkgs, ... }:
 {
-  home.packages = with pkgs; [ discord ];
-  # imports = [
-  #   inputs.nixcord.homeModules.nixcord
-  # ];
-  # programs.nixcord = {
-  #   enable = true;
-  #   discord.autoscroll.enable = true;
-  #   config = {
-  #     frameless = true;
-  #     disableMinSize = true;
-  #     plugins = {
-  #       callTimer.enable = true;
-  #       copyFileContents.enable = true;
-  #       fullUserInChatbox.enable = true;
-  #       iLoveSpam.enable = true;
-  #       imageZoom.enable = true;
-  #       implicitRelationships.enable = true;
-  #       mentionAvatars.enable = true;
-  #       messageLinkEmbeds.enable = true;
-  #       messageLogger.enable = true;
-  #       # mutualGroupDMs.enable = true;
-  #       noUnblockToJump.enable = true;
-  #       permissionsViewer.enable = true;
-  #       pinDMs.enable = true;
-  #       previewMessage.enable = true;
-  #       relationshipNotifier.enable = true;
-  #       revealAllSpoilers.enable = true;
-  #       serverInfo.enable = true;
-  #       showHiddenChannels.enable = true;
-  #       showHiddenThings.enable = true;
-  #       silentTyping.enable = true;
-  #       typingIndicator.enable = true;
-  #       unlockedAvatarZoom.enable = true;
-  #       userVoiceShow.enable = true;
-  #       vencordToolbox.enable = true;
-  #       viewRaw.enable = true;
-  #       voiceDownload.enable = true;
-  #       voiceMessages.enable = true;
-  #       volumeBooster.enable = true;
-  #       youtubeAdblock.enable = true;
-  #     };
-  #   };
-  # };
+
+  # home.packages = with pkgs; [ discord ];
+
+  imports = [
+    inputs.nixcord.homeModules.nixcord
+  ];
+  programs.nixcord = {
+    enable = true;
+    discord.autoscroll.enable = true;
+    config = {
+      frameless = true;
+      disableMinSize = true;
+      plugins = {
+        callTimer.enable = true;
+        copyFileContents.enable = true;
+        fullUserInChatbox.enable = true;
+        iLoveSpam.enable = true;
+        imageZoom.enable = true;
+        implicitRelationships.enable = true;
+        mentionAvatars.enable = true;
+        messageLinkEmbeds.enable = true;
+        messageLogger.enable = true;
+        # mutualGroupDMs.enable = true;
+        noUnblockToJump.enable = true;
+        permissionsViewer.enable = true;
+        pinDMs.enable = true;
+        previewMessage.enable = true;
+        relationshipNotifier.enable = true;
+        revealAllSpoilers.enable = true;
+        serverInfo.enable = true;
+        showHiddenChannels.enable = true;
+        showHiddenThings.enable = true;
+        silentTyping.enable = true;
+        typingIndicator.enable = true;
+        unlockedAvatarZoom.enable = true;
+        userVoiceShow.enable = true;
+        vencordToolbox.enable = true;
+        viewRaw.enable = true;
+        voiceDownload.enable = true;
+        voiceMessages.enable = true;
+        volumeBooster.enable = true;
+        youtubeAdblock.enable = true;
+      };
+    };
+  };
+
 }
