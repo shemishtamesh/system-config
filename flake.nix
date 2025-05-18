03c90b30f034e579b7a357d83a258cfb13714cf8
shemishtamesh <shemishtamail@gmail.com>
2025-05-18 15:16:07 +0300

    before formatting

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland = {
