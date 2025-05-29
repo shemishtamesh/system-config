b0a4c58e84a974133af2427e8dd091890c0d222c
shemishtamesh <shemishtamail@gmail.com>
2025-05-29 21:01:55 +0300

    system switch succeeded

--- config/hosts/shenixtamesh/configuration/services.nix
+++ config/hosts/shenixtamesh/configuration/services.nix
@@ -13,7 +13,7 @@
       enable = true;
 
       desktopManager.gnome.enable = true;
-      # displayManager.gdm.enable = true;
+      displayManager.gdm.enable = true;
 
       xkb = {
         # Configure keymap in X11
