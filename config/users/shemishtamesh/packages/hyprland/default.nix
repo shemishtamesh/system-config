ae27b440a9627352666ec81584c19f0508bf2a97
shemishtamesh <shemishtamail@gmail.com>
2025-05-31 12:26:45 +0300

    before formatting

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -261,6 +261,7 @@ in
         device = {
           name = "wacom-one-by-wacom-s-pen";
           output = builtins.elemAt sorted_monitors (builtins.length sorted_monitors / 2);
+          tablet.left_handed = true;
         };
         exec-once = [
           "${pkgs.hyprpaper}"
