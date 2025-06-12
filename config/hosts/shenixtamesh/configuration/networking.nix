b963461702f73ac27fd9a4c7843c4bc2d1f2921c
shemishtamesh <shemishtamail@gmail.com>
2025-06-12 06:18:02 +0300

    os config switch failed

--- config/hosts/shenixtamesh/configuration/networking.nix
+++ config/hosts/shenixtamesh/configuration/networking.nix
@@ -3,10 +3,10 @@
     networkmanager.enable = true;
 
     nameservers = [
-      "94.140.14.14"
-      "94.140.15.15"
-      "2a10:50c0::ad1:ff"
-      "2a10:50c0::ad2:ff"
+      # "94.140.14.14"
+      # "94.140.15.15"
+      # "2a10:50c0::ad1:ff"
+      # "2a10:50c0::ad2:ff"
       "1.1.1.1"
     ];
     dhcpcd.extraConfig = "nohook resolv.conf";
