81d648584d54974b883256fc0526140133c4a0e9
shemishtamesh <shemishtamail@gmail.com>
2025-06-13 11:42:20 +0300

    system switch succeeded

--- config/hosts/shenixtamesh/configuration/services.nix
+++ config/hosts/shenixtamesh/configuration/services.nix
@@ -8,12 +8,12 @@
       KERNEL=="i2c-[0-9]*", GROUP="wheel", MODE="0660"
     ''; # external monitor brightness control
 
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+
     xserver = {
       # Enable the X11 windowing system.
       enable = true;
-
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
 
       xkb = {
         # Configure keymap in X11
