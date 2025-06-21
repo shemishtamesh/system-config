8c59bcfe2b0df9c44abfa3e12f6dbb797da362a6
shemishtamesh <shemishtamail@gmail.com>
2025-06-21 13:53:18 +0300

    before formatting

--- config/users/shemishtamesh/packages/anyrun.nix
+++ config/users/shemishtamesh/packages/anyrun.nix
@@ -24,7 +24,7 @@
         # An array of all the plugins you want, which either can be paths to the .so files, or their packages
         inputs.anyrun.packages.${pkgs.system}.applications
         # ./some_plugin.so
-        "${inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins}/lib/kidex"
+        # "${inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins}/lib/kidex"
       ];
     };
 
