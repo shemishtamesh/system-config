e84efcff2360116aab84daaf7f7382405b99c42f
shemishtamesh <shemishtamail@gmail.com>
2025-06-21 16:44:20 +0300

    home config switch failed

--- config/users/shemishtamesh/packages/astal/default.nix
+++ config/users/shemishtamesh/packages/astal/default.nix
@@ -6,14 +6,17 @@
   programs.ags = {
     enable = true;
     configDir = ./source;
-    extraPackages = [
-      inputs.ags.packages.${host.system}.hyprland
-      inputs.ags.packages.${host.system}.mpris
-      inputs.ags.packages.${host.system}.battery
-      inputs.ags.packages.${host.system}.wp
-      inputs.ags.packages.${host.system}.network
-      inputs.ags.packages.${host.system}.tray
-      # pkgs.fzf
+    extraPackages = inputs.ags.packages.${host.system} [
+      tray
+      bluetooth
+      network
+      battery
+      wireplumber
+      hyprland
+      notifd
+      mpris
+      powerprofiles
+      apps
     ];
   };
 }
