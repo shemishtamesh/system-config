f04c25d00501115f734cb7129a7cc3fb96678c75
shemishtamesh <shemishtamail@gmail.com>
2025-06-21 14:35:33 +0300

    before formatting

--- config/users/shemishtamesh/packages/anyrun.nix
+++ config/users/shemishtamesh/packages/anyrun.nix
@@ -1,57 +1,3 @@
-# { inputs, pkgs, ... }:
-# {
-#   programs.anyrun = {
-#     enable = true;
-#     config = {
-#       x = {
-#         fraction = 0.5;
-#       };
-#       y = {
-#         fraction = 0.3;
-#       };
-#       width = {
-#         fraction = 0.3;
-#       };
-#       hideIcons = false;
-#       ignoreExclusiveZones = false;
-#       layer = "overlay";
-#       hidePluginInfo = false;
-#       closeOnClick = true;
-#       showResultsImmediately = true;
-#       maxEntries = null;
-#
-#       plugins = with inputs.anyrun.packages.${pkgs.system}; [
-#         applications
-#         dictionary
-#         kidex
-#         randr
-#         rink
-#         shell
-#         stdin
-#         symbols
-#         translate
-#         websearch
-#       ];
-#     };
-#
-#     extraCss = # css
-#       ''
-#         .some_class {
-#           background: red;
-#         }
-#       '';
-#
-#     extraConfigFiles."some-plugin.ron".text = # ron
-#       ''
-#         Config(
-#           prefix: ":",
-#           language_delimiter: ">",
-#           max_entries: 3,
-#         )
-#       '';
-#   };
-# }
-
 {
   pkgs,
   inputs,
@@ -63,9 +9,15 @@
     config = {
       plugins = with inputs.anyrun.packages.${pkgs.system}; [
         applications
+        dictionary
+        kidex
+        randr
+        rink
         shell
+        stdin
         symbols
         translate
+        websearch
       ];
 
       width.fraction = 0.25;
