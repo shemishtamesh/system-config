a0b5e11542d07b79ea88a568271fe67c73b7c73a
shemishtamesh <shemishtamail@gmail.com>
2025-06-21 20:34:34 +0300

    before formatting

--- config/users/shemishtamesh/packages/astal/default.nix
+++ config/users/shemishtamesh/packages/astal/default.nix
@@ -9,36 +9,45 @@
   programs.ags = {
     enable = true;
     configDir = ./source;
-    extraPackages =
-      (with inputs.ags.packages.${host.system}; [
-        hyprland
-        network
-        powerprofiles
-        wireplumber
-        battery
-        bluetooth
-        notifd
-        astal4
-        astal3
-        mpris
-        greet
-        river
-        auth
-        tray
-        apps
-        cava
-        gjs
-        io
-      ])
-      ++ (with pkgs; [
-        meson
-        ninja
-        pkg-config
-        gobject-introspection
-        wrapGAppsHook4
-        blueprint-compiler
-        dart-sass
-        esbuild
-      ]);
+    extraPackages = with inputs.ags.packages.${host.system}; [
+      hyprland
+      network
+      powerprofiles
+      wireplumber
+      battery
+      bluetooth
+      notifd
+      astal4
+      astal3
+      mpris
+      greet
+      river
+      auth
+      tray
+      apps
+      cava
+      gjs
+      io
+    ];
   };
+  home.packages = with inputs.ags.packages.${host.system}; [
+    hyprland
+    network
+    powerprofiles
+    wireplumber
+    battery
+    bluetooth
+    notifd
+    astal4
+    astal3
+    mpris
+    greet
+    river
+    auth
+    tray
+    apps
+    cava
+    gjs
+    io
+  ];
 }
