229158c81a5034ea3c0597567c1be193e2281cff
shemishtamesh <shemishtamail@gmail.com>
2025-06-21 13:58:26 +0300

    before formatting

--- config/users/shemishtamesh/packages/anyrun.nix
+++ config/users/shemishtamesh/packages/anyrun.nix
@@ -36,13 +36,5 @@
           background: red;
         }
       '';
-
-    extraConfigFiles."some-plugin.ron".text = ''
-      Config(
-        // for any other plugin
-        // this file will be put in ~/.config/anyrun/some-plugin.ron
-        // refer to docs of xdg.configFile for available options
-      )
-    '';
   };
 }
