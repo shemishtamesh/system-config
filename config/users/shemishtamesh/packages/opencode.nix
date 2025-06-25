ac7cde829b7f74dd586f1d2b07e5d1453dcee129
shemishtamesh <shemishtamail@gmail.com>
2025-06-25 12:48:17 +0300

    home config switch failed

---
+++ config/users/shemishtamesh/packages/opencode.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [ opencode ];
+  xdg.configFile.source = # json
+    ''
+      {
+        "$schema": "https://opencode.ai/config.json",
+        "provider": {
+          "ollama": {
+            "npm": "@ai-sdk/openai-compatible",
+            "options": {
+              "baseURL": "http://localhost:11434/v1"
+            },
+            "models": {
+              "llama2": {}
+            }
+          }
+        }
+      }
+    '';
+}
