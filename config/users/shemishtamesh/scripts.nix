922ef2d0bf2ce3f9ad5c8576523522d059b167eb
Yuval Inov <yuval@agamanalytics.com>
2025-07-09 09:45:14 +0300

    before formatting

--- config/users/shemishtamesh/scripts.nix
+++ config/users/shemishtamesh/scripts.nix
@@ -43,7 +43,7 @@
             fi
             current_status=$(echo "1 - $(pmset -g | grep SleepDisabled | awk '{ print $2 }')" | bc)
             sudo pmset -a disablesleep $current_status in
-            case $current_status
+            case $current_status in
               0) echo 'Sleep enabled';;
               1) echo 'Sleep disabled';;
             esac
