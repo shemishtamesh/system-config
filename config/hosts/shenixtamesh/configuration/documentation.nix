62a8ec0395b35c527ef260b9d25639c51810f5b9
shemishtamesh <shemishtamail@gmail.com>
2025-07-12 21:38:22 +0300

    system switch succeeded

--- config/hosts/shenixtamesh/configuration/documentation.nix
+++ config/hosts/shenixtamesh/configuration/documentation.nix
@@ -6,6 +6,6 @@
   documentation = {
     dev.enable = true;
     man.generateCaches = true;
-    nixos.includeAllModules = true;
+    # nixos.includeAllModules = true; # https://github.com/nix-community/stylix/issues/98
   };
 }
