786637f28dd4288ebdffed75f8d9725555d223f1
shemishtamesh <shemishtamail@gmail.com>
2025-07-12 21:28:33 +0300

    before formatting

--- config/hosts/shenixtamesh/configuration/documentation.nix
+++ config/hosts/shenixtamesh/configuration/documentation.nix
@@ -4,7 +4,7 @@
   # environment.systemPackages = [ pkgs.man-pages ];
 
   documentation = {
-    # dev.enable = true;
+    dev.enable = true;
     man.generateCaches = true;
     # nixos.includeAllModules = true;
   };
