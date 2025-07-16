368e40edc2143cb774f3b5b7d61f54aa2fd50317
shemishtamesh <shemishtamail@gmail.com>
2025-07-16 08:09:10 +0300

    before formatting

--- config/hosts/shenixtamesh/configuration/networking.nix
+++ config/hosts/shenixtamesh/configuration/networking.nix
@@ -3,19 +3,23 @@
     networkmanager.enable = true;
 
     nameservers = [
+      # adguard
       # "94.140.14.14"
       # "94.140.15.15"
       # "2a10:50c0::ad1:ff"
       # "2a10:50c0::ad2:ff"
-      "1.1.1.1"
-      "8.8.8.8"
+
+      "1.1.1.1" # cloudflair
+      "8.8.8.8" # google
     ];
     dhcpcd.extraConfig = "nohook resolv.conf";
     networkmanager.dns = "none";
 
     # Open ports in the firewall.
     firewall = {
-      allowedTCPPorts = [ 8080 ];
+      allowedTCPPorts = [
+        8080 # open-webui
+      ];
       allowedTCPPortRanges = [
         {
           # KDE Connect
