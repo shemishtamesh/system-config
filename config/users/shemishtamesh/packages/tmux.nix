aa458f0b5dae8d6a386695c907342823160b9711
shemishtamesh <shemishtamail@gmail.com>
2025-08-19 21:23:29 +0300

    before formatting

--- config/users/shemishtamesh/packages/tmux.nix
+++ config/users/shemishtamesh/packages/tmux.nix
@@ -204,7 +204,7 @@ in
             --bind 'ctrl-g:change-prompt(⚙️  )+reload(${sesh} list -c --icons)' \
             --bind 'ctrl-x:change-prompt(📁  )+reload(${sesh} list -z --icons)' \
             --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+            --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(${sesh} list --icons)' \
             --preview-window 'right:55%' \
             --preview '${sesh} preview {}'
         )\""
