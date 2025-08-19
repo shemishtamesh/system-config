31844839bc98534815af33585ef0f1ef0764d7f4
shemishtamesh <shemishtamail@gmail.com>
2025-08-19 23:03:41 +0300

    before formatting

--- config/shared/constants.nix
+++ config/shared/constants.nix
@@ -1,4 +1,9 @@
-pkgs: {
+pkgs:
+let
   FLAKE_ROOT = "$HOME/.config/system";
+in
+{
+  inherit FLAKE_ROOT;
+  FLAKE_ROOT_TILDE = pkgs.lib.replaceString "$HOME" "~" FLAKE_ROOT;
   FLAKE_REPO = "https://github.com/shemishtamesh/system-config.git";
 }
--- config/users/shemishtamesh/packages/tmux.nix
+++ config/users/shemishtamesh/packages/tmux.nix
@@ -243,14 +243,13 @@ in
 
       [[session]]
       name = "configuration"
-      path = "${shared.constants.FLAKE_ROOT}"
+      path = "${shared.constants.FLAKE_ROOT_TILDE}"
       startup_command = "nvim flake.nix"
-      # preview_command = "bat --color=always ${shared.constants.FLAKE_ROOT}/flake.nix"
-      preview_command = "bat --color=always ~/.config/system/flake.nix"
+      preview_command = "bat --color=always ${shared.constants.FLAKE_ROOT_TILDE}/flake.nix"
       windows = [ "system", "nixvim" ]
 
       [[window]]
       name = "nixvim"
-      path = "$HOME/.config/nixvim"
+      path = "~/.config/nixvim"
     '';
 }
