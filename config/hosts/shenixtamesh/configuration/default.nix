c19a7142441074beb1baf043a4c946f0bebe1b16
shemishtamesh <shemishtamail@gmail.com>
2025-09-09 17:24:23 +0300

    os config switch failed

--- config/hosts/shenixtamesh/configuration/default.nix
+++ config/hosts/shenixtamesh/configuration/default.nix
@@ -29,6 +29,8 @@
   hardware = {
     keyboard.qmk.enable = true;
 
+    pulseaudio.enable = true;
+
     bluetooth.enable = true; # enables support for Bluetooth
     bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
