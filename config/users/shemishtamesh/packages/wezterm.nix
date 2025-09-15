1014e9ecbdf0d067501ae6654d90ea7fdba8147d
shemishtamesh <shemishtamail@gmail.com>
2025-09-15 21:47:37 +0300

    before formatting

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -4,6 +4,8 @@
     extraConfig = # lua
       ''
         return {
+          bidi_enabled = true
+          bidi_direction = "LeftToRight"
           hide_tab_bar_if_only_one_tab = true
           font_size = 14.0,
         }
