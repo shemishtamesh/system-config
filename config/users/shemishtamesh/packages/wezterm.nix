8526daf9e00d3e030c57c4509aec3532e01436c1
shemishtamesh <shemishtamail@gmail.com>
2025-09-15 21:40:28 +0300

    before formatting

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -3,12 +3,7 @@
     enable = true;
     extraConfig = # lua
       ''
-        local wezterm = require 'wezterm'
-        local config = wezterm.config_builder()
-
         config.hide_tab_bar_if_only_one_tab = true
-
-        return config
       '';
   };
 }
