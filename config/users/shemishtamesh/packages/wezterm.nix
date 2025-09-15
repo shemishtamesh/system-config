8226228ce73e9851997ca7858da703d2f766bfd4
shemishtamesh <shemishtamail@gmail.com>
2025-09-15 21:48:15 +0300

    before formatting

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -4,9 +4,9 @@
     extraConfig = # lua
       ''
         return {
-          bidi_enabled = true
-          bidi_direction = "LeftToRight"
-          hide_tab_bar_if_only_one_tab = true
+          bidi_enabled = true,
+          bidi_direction = "LeftToRight",
+          hide_tab_bar_if_only_one_tab = true,
           font_size = 14.0,
         }
       '';
