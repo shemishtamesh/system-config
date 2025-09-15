92903985983535cb46ca2409b3adecb599e237c4
shemishtamesh <shemishtamail@gmail.com>
2025-09-15 21:42:54 +0300

    before formatting

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -3,8 +3,9 @@
     enable = true;
     extraConfig = # lua
       ''
-        asodifjaoweijfawconfig.hide_tab_bar_if_only_one_tab = true
-        awifelawejflaiwej
+        return {
+          hide_tab_bar_if_only_one_tab = true
+        }
       '';
   };
 }
