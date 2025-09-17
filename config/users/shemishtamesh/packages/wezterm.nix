2191f75d7d76d6731af23b5bc69673f4b93d98d7
shemishtamesh <shemishtamail@gmail.com>
2025-09-17 07:53:38 +0300

    before formatting

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -6,8 +6,14 @@
         return {
           bidi_enabled = true,
           bidi_direction = "LeftToRight",
-          hide_tab_bar_if_only_one_tab = true,
           font_size = 14.0,
+          hide_tab_bar_if_only_one_tab = true,
+          window_padding = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0,
+          }
         }
       '';
   };
