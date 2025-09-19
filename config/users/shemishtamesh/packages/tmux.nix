bedd68a63f6816c777a1dc43bdd2d5d8c750e55b
shemishtamesh <shemishtamail@gmail.com>
2025-09-19 11:53:06 +0300

    before formatting

--- config/users/shemishtamesh/packages/tmux.nix
+++ config/users/shemishtamesh/packages/tmux.nix
@@ -201,8 +201,7 @@ in
 
         # allow passthrough to let images render correctly with presenterm and nvim zen mode in wezterm
         set -g allow-passthrough on
-        # set -as terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[%p1%d q'
-        set -as terminal-overrides ',*:Ss=\E[%p:Se=\E[%p'
+        set -as terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[%p1%d q'
 
         # start new panes and windows in the same directory
         bind '"' split-window -c "#{pane_current_path}"
