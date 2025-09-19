26907e81edd569094c31308cd7ce201342b32023
shemishtamesh <shemishtamail@gmail.com>
2025-09-19 11:14:04 +0300

    before formatting

--- config/users/shemishtamesh/packages/zsh.nix
+++ config/users/shemishtamesh/packages/zsh.nix
@@ -93,13 +93,13 @@ in
           fi
         }
         zle -N zle-keymap-select
-        # zle-line-init() {
-        #   zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-        #   echo -ne "\e[5 q"
-        # }
-        # zle -N zle-line-init
-        # echo -ne '\e[5 q' # Use beam shape cursor on startup.
-        # preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+        zle-line-init() {
+          zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+          echo -ne "\e[5 q"
+        }
+        zle -N zle-line-init
+        echo -ne '\e[5 q' # Use beam shape cursor on startup.
+        preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
         # Edit line in vim with ctrl-e:
         autoload edit-command-line; zle -N edit-command-line
