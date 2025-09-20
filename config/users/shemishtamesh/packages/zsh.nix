7b7c5acc0ac6abee7140b70c71938f77d077732e
Yuval Inov <yuval@agamanalytics.com>
2025-09-20 11:12:06 +0300

    before formatting

--- config/users/shemishtamesh/packages/zsh.nix
+++ config/users/shemishtamesh/packages/zsh.nix
@@ -86,23 +86,22 @@ in
         export KEYTIMEOUT=1
         # Change cursor shape for different vi modes.
         # export ZVM_CURSOR_STYLE_ENABLED=false # disable plugin's cursor style
-        export ZVM_CURSOR_STYLE_ENABLED=true # disable plugin's cursor style
-        function zle-keymap-select {
-          if [[ ''${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
-            echo -ne '\e[1 q'
-          elif [[ ''${KEYMAP} == main ]] || [[ ''${KEYMAP} == viins ]] \
-            || [[ ''${KEYMAP} = "" ]] || [[ $1 = 'beam' ]]; then
-            echo -ne '\e[5 q'
-          fi
-        }
-        zle -N zle-keymap-select
-        zle-line-init() {
-          zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-          echo -ne "\e[5 q"
-        }
-        zle -N zle-line-init
-        echo -ne '\e[5 q' # Use beam shape cursor on startup.
-        preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+        # function zle-keymap-select {
+        #   if [[ ''${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+        #     echo -ne '\e[1 q'
+        #   elif [[ ''${KEYMAP} == main ]] || [[ ''${KEYMAP} == viins ]] \
+        #     || [[ ''${KEYMAP} = "" ]] || [[ $1 = 'beam' ]]; then
+        #     echo -ne '\e[5 q'
+        #   fi
+        # }
+        # zle -N zle-keymap-select
+        # zle-line-init() {
+        #   zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+        #   echo -ne "\e[5 q"
+        # }
+        # zle -N zle-line-init
+        # echo -ne '\e[5 q' # Use beam shape cursor on startup.
+        # preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
         # Edit line in vim with ctrl-e:
         autoload edit-command-line; zle -N edit-command-line
