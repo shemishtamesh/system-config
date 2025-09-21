de2ed9b4a7afff67c25a8ccb1dce40258266f254
shemishtamesh <shemishtamail@gmail.com>
2025-09-21 08:17:03 +0300

    switch home config

--- config/users/shemishtamesh/packages/tattoy.nix
+++ config/users/shemishtamesh/packages/tattoy.nix
@@ -4,16 +4,16 @@
   xdg.configFile = {
     "tattoy/tattoy.toml".text = # toml
       ''
-      show_startup_logo = false
-      show_tattoy_indicator = false
+        show_startup_logo = false
+        show_tattoy_indicator = false
 
-        scrollback_size = 10000
+          scrollback_size = 10000
 
-        [animated_cursor]
-        enabled = true
-        opacity = 1.0
-        path = "shaders/cursors/smear_fade.glsl"
-        cursor_scale = 0.0
+          [animated_cursor]
+          enabled = true
+          opacity = 1.0
+          path = "shaders/cursors/smear_fade.glsl"
+          cursor_scale = 0.0
       '';
   };
 }
