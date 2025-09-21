aac36c25a97e7534733a31e9684458aaf6983139
shemishtamesh <shemishtamail@gmail.com>
2025-09-21 08:24:14 +0300

    before formatting

--- config/users/shemishtamesh/packages/tattoy.nix
+++ config/users/shemishtamesh/packages/tattoy.nix
@@ -4,16 +4,17 @@
   xdg.configFile = {
     "tattoy/tattoy.toml".text = # toml
       ''
+        log_level = "debug"
         show_startup_logo = false
         show_tattoy_indicator = false
 
-          scrollback_size = 10000
+        scrollback_size = 10000
 
-          [animated_cursor]
-          enabled = true
-          opacity = 1.0
-          path = "shaders/cursors/smear_fade.glsl"
-          cursor_scale = 0.0
+        [animated_cursor]
+        enabled = true
+        opacity = 1.0
+        path = "shaders/cursors/smear_fade.glsl"
+        cursor_scale = 0.0
       '';
   };
 }
