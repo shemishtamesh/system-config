6b7dc69b93df5e20e891a02c708f222105a9628f
shemishtamesh <shemishtamail@gmail.com>
2025-10-18 19:13:06 +0300

    before formatting

--- config/shared/theming/wallpaper_generator.py
+++ config/shared/theming/wallpaper_generator.py
@@ -232,7 +232,7 @@ class Background:
                     1,
                 )
                 if not color_outside_nix:
-                    color_index = min(color_index, 6)  # 8 would be brightest
+                    color_index = min(color_index, 3)  # 8 would be brightest
                 self.triangles.append(
                     triangle_factory.triangle(
                         Point(x, y),
