c5846d8383e3176ecaeb7b7e118270d2a1e8d65e
shemishtamesh <shemishtamail@gmail.com>
2025-10-18 13:33:12 +0300

    before formatting

--- config/shared/theme.nix
+++ config/shared/theme.nix
@@ -83,6 +83,7 @@ in
           ${if !nix then "--no_nix" else ""} \
           ${if !gaps then "--gaps 0" else ""} \
           ${if !random then "--no_random" else ""} \
+          --distance_fade_scale "0.25" \
           --no_color_outside_nix \
       '';
       installPhase = "install -Dm0644 ${name} $out";
