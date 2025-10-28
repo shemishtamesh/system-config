c9b7946c8d66e149aeebdfb91418fd69d0f2ca14
shemishtamesh <shemishtamail@gmail.com>
2025-10-28 08:44:21 +0200

    os config switch failed

--- config/users/shemishtamesh/packages/zsh.nix
+++ config/users/shemishtamesh/packages/zsh.nix
@@ -102,7 +102,7 @@ in
         zle -N zle-line-init
         # Ensure cursor shape is restored after every command (after exiting nvim, etc.)
         precmd() {
-          echo -ne '\e[2 q'  # default to block cursor on prompt
+          echo -ne '\e[5 q'  # default to block cursor on prompt
         }
         echo -ne '\e[5 q' # Use beam shape cursor on startup.
         preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
