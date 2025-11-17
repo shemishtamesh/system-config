207743b6ba8a64744c6f91f44fe0ba197befbf0c
Yuval Inov <yuval@agamanalytics.com>
2025-11-17 13:12:51 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/direnv.nix
+++ config/users/shemishtamesh/packages/direnv.nix
@@ -1,6 +1,21 @@
 {
+  inputs,
+  host,
+  pkgs,
+  ...
+}:
+let
+  stable-pkgs = import inputs.nixpkgs-stable {
+    system = host.system;
+    config.allowUnfree = true;
+  };
+in
+{
   programs = {
-    direnv.enable = true;
+    direnv = {
+      enable = true;
+      package = if host.system != "aarch64-darwin" then pkgs.direnv else stable-pkgs.direnv;
+    };
     zsh.initContent = # sh
       ''
         # ensure compatibility tmux <-> direnv
