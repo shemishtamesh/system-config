baee24ef94bb78f6aa9a0848392a35adaaf0ee7c
shemishtamesh <shemishtamail@gmail.com>
2025-11-25 09:36:04 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -261,7 +261,7 @@ in
         };
         windowrule = [
           # open aseprite in tiled mode by default
-          "tile, match:class Aseprite"
+          "tile:true, match:class Aseprite"
         ];
         workspace = [
           # no borders when there's only a single visible window
