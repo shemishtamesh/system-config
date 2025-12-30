edfa3cdc7fc997cf98183e7fbbf88c1bd501b381
shemishtamesh <shemishtamail@gmail.com>
2025-12-30 16:06:00 +0200

    os config switch failed

--- config/users/shemishtamesh/environment_variables.nix
+++ config/users/shemishtamesh/environment_variables.nix
@@ -1,8 +1,15 @@
-{ host, lib, ... }:
+{
+  host,
+  lib,
+  pkgs,
+  ...
+}:
 let
   system_type = lib.last (lib.splitString "-" host.system);
 in
 {
-  home.sessionVariables =
-    { } // (if system_type == "darwin" then { HOMEBREW_NO_ANALYTICS = 1; } else { });
+  home.sessionVariables = {
+    FLAKE = (import ../../shared/constants.nix pkgs).FLAKE_ROOT;
+  }
+  // (if system_type == "darwin" then { HOMEBREW_NO_ANALYTICS = 1; } else { });
 }
