fac2667bd01465bcfec10443cd4859640a4461ea
shemishtamesh <shemishtamail@gmail.com>
2026-01-02 18:21:34 +0200

    system switch succeeded

--- config/users/shemishtamesh/environment_variables.nix
+++ config/users/shemishtamesh/environment_variables.nix
@@ -10,10 +10,10 @@ in
 {
   home.sessionVariables = {
     FLAKE = (import ../../shared/constants.nix pkgs).FLAKE_ROOT;
-    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_STATE_HOME  = "$HOME/.local/state";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
   }
   // (if system_type == "darwin" then { HOMEBREW_NO_ANALYTICS = 1; } else { });
 }
