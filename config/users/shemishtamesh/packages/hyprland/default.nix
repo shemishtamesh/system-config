376b9226ac41cc255226b3ebcfd174bdbc5b0c85
shemishtamesh <shemishtamail@gmail.com>
2026-01-02 18:36:59 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -260,7 +260,7 @@ in
           force_default_wallpaper = 1;
           disable_hyprland_logo = false;
         };
-        layerrule = [ "noanim, ^quickshell$" ];
+        layerrule = [ "animations:enabled 0, quickshell" ];
         windowrule = [
           # open aseprite in tiled mode by default
           "match:class Aseprite, tile true"
