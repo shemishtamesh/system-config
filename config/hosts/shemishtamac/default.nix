12b5ee0bb4bd31dbce50ad33c001ad8c5013d756
Yuval Inov <yuval@agamanalytics.com>
2026-01-05 09:53:28 +0200

    system switch succeeded

--- config/hosts/shemishtamac/default.nix
+++ config/hosts/shemishtamac/default.nix
@@ -3,4 +3,10 @@ profile_makers.mkDarwinSystem {
   hostname = "shemishtamac";
   system = "aarch64-darwin";
   users.shemishtamesh = { };
+  monitors = {
+    "BUILTIN" = {
+      width = 3456;
+      height = 2234;
+    };
+  };
 }
