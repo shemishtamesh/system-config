2a8f3aeaca45f19621cf0e70a3d5b14ac2440e53
shemishtamesh <shemishtamail@gmail.com>
2026-01-07 09:08:39 +0200

    before formatting

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -213,7 +213,7 @@ in
 
           resize_on_border = true;
 
-          "col.active_border" = lib.mkForce "0x${config.lib.stylix.colors.base05}0F";
+          "col.active_border" = lib.mkForce "0x${config.lib.stylix.colors.base05}00";
           "col.inactive_border" = lib.mkForce "0x00000000"; # transparent
         };
         cursor = {
