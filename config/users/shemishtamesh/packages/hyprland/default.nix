ae97f5ca4342628e2f78fe735e9a30d39e53d78a
shemishtamesh <shemishtamail@gmail.com>
2026-01-07 09:07:35 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -213,7 +213,7 @@ in
 
           resize_on_border = true;
 
-          "col.active_border" = lib.mkForce "0x${config.lib.stylix.colors.base05}7F";
+          "col.active_border" = lib.mkForce "0x${config.lib.stylix.colors.base05}0F";
           "col.inactive_border" = lib.mkForce "0x00000000"; # transparent
         };
         cursor = {
