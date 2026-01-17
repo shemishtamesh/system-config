69c85f37427c298768709d29d167c39ae9ca6668
shemishtamesh <shemishtamail@gmail.com>
2026-01-17 19:36:01 +0200

    system switch succeeded

--- config/hosts/shenixtamesh/configuration/services.nix
+++ config/hosts/shenixtamesh/configuration/services.nix
@@ -61,18 +61,16 @@ in
       openFirewall = true;
       environmentVariables = {
         OLLAMA_HOST = "0.0.0.0";
+        OLLAMA_CONTEXT_LENGTH = "65536";
+        OLLAMA_FLASH_ATTENTION = "1";
+        OLLAMA_KV_CACHE_TYPE = "q8_0";
       };
       loadModels = [ "qwen2.5-coder:7b" ];
     };
     open-webui = {
       enable = true;
       package = stable-pkgs.open-webui;
-      environment = {
-        OLLAMA_API_BASE_URL = "http://localhost:11434";
-        OLLAMA_CONTEXT_LENGTH = "65536";
-        OLLAMA_FLASH_ATTENTION = "1";
-        OLLAMA_KV_CACHE_TYPE = "q8_0";
-      };
+      environment.OLLAMA_API_BASE_URL = "http://localhost:11434";
       host = "0.0.0.0";
     };
   };
