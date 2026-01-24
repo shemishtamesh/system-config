412529baba8d2d5deaef52b4040515ddfa255d89
shemishtamesh <shemishtamail@gmail.com>
2026-01-24 10:15:56 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/noctalia.nix
+++ config/users/shemishtamesh/packages/noctalia.nix
@@ -24,7 +24,7 @@
           ${noctalia-package} "$@"
           exit 0
         fi
-        ${noctalia-package} "$@"
+        ${noctalia-package} -d "$@"
         sleep 3 # set location doesn't seem to work immediately
         ${noctalia-package} ipc call location set "$(cat ${config.sops.secrets.location.path})"
       '');
