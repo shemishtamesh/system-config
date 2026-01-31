e05a74d372adf8a247181274b0301172b6056010
shemishtamesh <shemishtamail@gmail.com>
2026-01-31 13:30:26 +0200

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -219,6 +219,7 @@ in
         cursor = {
           hide_on_key_press = true;
           zoom_disable_aa = true;
+          no_hardware_cursors = 0;
         };
         ecosystem = {
           no_update_news = true;
