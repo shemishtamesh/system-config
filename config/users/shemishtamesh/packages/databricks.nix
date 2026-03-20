4882edf9ff750605f10a705d2bd0868b311e4e9b
shemishtamesh <shemishtamail@gmail.com>
2026-03-20 17:30:45 +0200

    home config switch failed

--- config/users/shemishtamesh/packages/databricks.nix
+++ config/users/shemishtamesh/packages/databricks.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
-
+let
+  databricks-cli = pkgs.databricks-cli;
+in
 {
-  home = let databricks-cli = pkgs.databricks-cli {
-    packages = [ databricks-cli ];
-    file.".zsh/completions/_databricks".source =
-      let
-        databricksZshCompletion = pkgs.runCommand "databricks-zsh-completion" { } ''
-          mkdir -p "$out"
-          ${databricks-cli}/bin/databricks completion zsh > "$out/_databricks"
-        '';
-      in
-      "${databricksZshCompletion}/_databricks";
-  };
+  home.packages = [ databricks-cli ];
+  programs.zsh.plugins.databricks-cli.completions =
+    let
+      databricksZshCompletion = pkgs.runCommand "databricks-zsh-completion" { } ''
+        mkdir -p "$out"
+        ${databricks-cli}/bin/databricks completion zsh > "$out/_databricks"
+      '';
+    in
+    "${databricksZshCompletion}/_databricks";
 }
