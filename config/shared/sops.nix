f5079fef8e30d73823d76ae0182588c7da7996bc
Yuval Inov <yuval@agamanalytics.com>
2026-03-22 00:29:26 +0200

    system switch succeeded

--- config/shared/sops.nix
+++ config/shared/sops.nix
@@ -23,4 +23,12 @@ in
       generateKey = true;
     };
   };
+  launchd.agents.sops-nix = pkgs.lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
+    config = {
+      EnvironmentVariables = {
+        PATH = pkgs.lib.mkForce "/usr/bin:/bin:/usr/sbin:/sbin";
+      };
+    };
+  };
 }
