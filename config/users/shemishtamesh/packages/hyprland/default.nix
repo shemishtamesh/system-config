e88cef93c26a11360d1506529965e19a5212e5bc
shemishtamesh <shemishtamail@gmail.com>
2026-03-29 17:12:53 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -274,7 +274,7 @@ in
           "match:initial_class (?i)^(discord|vesktop|altus|slack)$, workspace special:chat silent"
 
           # special workspace appearance
-          "match:workspace s[true], opacity 1 0.5"
+          "match:workspace s[true], opacity 0.9 0.5"
         ];
         workspace = [
           # no borders when there's only a single visible window
