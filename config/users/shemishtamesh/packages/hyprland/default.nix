a5f11500fe3fd7fa751d9b15a7f19a1d4a6049bd
shemishtamesh <shemishtamail@gmail.com>
2026-03-29 20:04:41 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -237,7 +237,7 @@ in
           inherit rounding;
           shadow = {
             enabled = true;
-            range = 20;
+            range = 30;
           };
         };
         dwindle = {
