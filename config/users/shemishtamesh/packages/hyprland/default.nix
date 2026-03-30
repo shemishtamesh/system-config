0a45727feb5dc47a97a9899e023e1cd179b305a0
shemishtamesh <shemishtamail@gmail.com>
2026-03-30 17:19:05 +0300

    os config switch failed

--- config/users/shemishtamesh/packages/hyprland/default.nix
+++ config/users/shemishtamesh/packages/hyprland/default.nix
@@ -278,7 +278,7 @@ in
           "match:initial_class (?i)^(discord|vesktop|altus|slack)$, workspace special:chat silent"
 
           # special workspace appearance
-          "match:workspace s[true], opacity 0.9 0.5"
+          "match:workspace s[true], opacity 0.9 0.7"
         ];
         workspace = [
           # no borders when there's only a single visible window
