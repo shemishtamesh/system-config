f6b5659aa0156065cd6c0401617478a7c760a261
shemishtamesh <shemishtamail@gmail.com>
2026-04-03 19:46:20 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/wezterm.nix
+++ config/users/shemishtamesh/packages/wezterm.nix
@@ -71,9 +71,9 @@ in
           },
           window_decorations = "${window_decorations}",
 
-          enable_cursor_animation = true
-          cursor_animation_duration_ms = 150
-          cursor_trail_opacity = 0.4
+          enable_cursor_animation = true,
+          cursor_animation_duration_ms = 150,
+          cursor_trail_opacity = 0.4,
         }
       '';
   };
