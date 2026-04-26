c8739c46fcaa062850e8822462e3c8fc45a85035
Yuval Inov <yuval@agamanalytics.com>
2026-04-26 08:47:59 +0300

    before formatting

--- config/users/shemishtamesh/packages/zathura.nix
+++ config/users/shemishtamesh/packages/zathura.nix
@@ -1,6 +1,18 @@
 {
+  inputs,
+  host,
+  ...
+}:
+let
+  stable-pkgs = import inputs.nixpkgs-stable {
+    inherit (host) system;
+    config.allowUnfree = true;
+  };
+in
+{
   programs.zathura = {
     enable = true;
+    package = stable-pkgs.zathura;
     options = {
       guioptions = "none";
     };
