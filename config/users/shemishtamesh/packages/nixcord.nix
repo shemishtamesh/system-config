a91eca33affe2cdab85917d7bf6e5b7e6caa0949
shemishtamesh <shemishtamail@gmail.com>
2026-05-12 12:30:59 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/nixcord.nix
+++ config/users/shemishtamesh/packages/nixcord.nix
@@ -14,7 +14,7 @@
         callTimer.enable = true;
         copyFileContents.enable = true;
         fullUserInChatbox.enable = true;
-        IloveSpam.enable = true;
+        iLoveSpam.enable = true;
         imageZoom.enable = true;
         implicitRelationships.enable = true;
         mentionAvatars.enable = true;
