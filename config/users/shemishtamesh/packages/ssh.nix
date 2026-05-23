69e59336c09431d93e00ad252812558b219a8703
shemishtamesh <shemishtamail@gmail.com>
2026-05-23 21:20:06 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/ssh.nix
+++ config/users/shemishtamesh/packages/ssh.nix
@@ -2,21 +2,20 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "yes";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
+    settings = {
+      "*" = {
+        ForwardAgent = false;
+        AddKeysToAgent = "yes";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
+        IdentityFile = "~/.ssh/id_ed25519";
+      };
     };
-    extraConfig = ''
-      Host *
-        IdentityFile ~/.ssh/id_ed25519
-    '';
   };
 }
