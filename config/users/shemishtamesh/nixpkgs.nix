de76575e4ef7cccb17461f4a33b3da00ab8ab699
shemishtamesh <shemishtamail@gmail.com>
2026-06-12 11:02:52 +0300

    home config switch failed

--- config/users/shemishtamesh/nixpkgs.nix
+++ config/users/shemishtamesh/nixpkgs.nix
@@ -3,10 +3,12 @@ let
   object = {
     allowUnfree = true;
     cudaSupport = if pkgs.stdenv.system == "aarch64-darwin" then false else true;
+    permittedInsecurePackages = [ "electron-39.8.10" ]; # TODO: remove this when https://github.com/NixOS/nixpkgs/issues/526914 is resolved
   };
   config_text = pkgs.lib.generators.toPretty { } object;
 in
 {
   inherit object;
   file = pkgs.writeText "nixpkgs.nix" config_text;
+
 }
