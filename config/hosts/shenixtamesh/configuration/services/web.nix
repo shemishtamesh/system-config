1d043d6a1f96fc2b18940b6de68969216102e68e
shemishtamesh <shemishtamail@gmail.com>
2026-06-26 10:52:03 +0300

    os config switch failed

--- config/hosts/shenixtamesh/configuration/services/web.nix
+++ config/hosts/shenixtamesh/configuration/services/web.nix
@@ -52,5 +52,24 @@
       enable = true;
       listenPort = 3030;
     };
+
+    networking = {
+      firewall = {
+        allowedTCPPortRanges = [
+          {
+            # kdeconnect
+            from = 1714;
+            to = 1764;
+          }
+        ];
+        allowedUDPPortRanges = [
+          {
+            # kdeconnect
+            from = 1714;
+            to = 1764;
+          }
+        ];
+      };
+    };
   };
 }
