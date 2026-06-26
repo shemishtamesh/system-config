2103564d6ff04076d19d11254ecc00b47cc35def
shemishtamesh <shemishtamail@gmail.com>
2026-06-26 14:53:43 +0300

    system switch succeeded

--- config/users/shemishtamesh/packages/silverbullet-plus.nix
+++ config/users/shemishtamesh/packages/silverbullet-plus.nix
@@ -9,7 +9,7 @@ in
       inherit version;
       src = pkgs.fetchurl {
         url = "https://releases.silverbullet.plus/releases/${version}/SilverBullet_x86_64.AppImage";
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        hash = "sha256-GtiLG6cnZIwwMGjINYEzboCTi+0weUX7zvrtmgA7y4c=";
       };
     })
   ];
