c91e635724317ae293e08d5b675d3b116e3597ef
shemishtamesh <shemishtamail@gmail.com>
2026-07-03 17:17:30 +0300

    before formatting

--- config/users/shemishtamesh/packages/atuin.nix
+++ config/users/shemishtamesh/packages/atuin.nix
@@ -1,15 +1,19 @@
 {
-  programs.atuin = {
-    enable = true;
-    flags = [ "--disable-up-arrow" ];
-    settings = {
-      keymap_mode = "vim-insert";
-      keymap_cursor = {
-        vim_insert = "blink-bar";
-        vim_normal = "steady-block";
+  programs = {
+    atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+      settings = {
+        keymap_mode = "vim-insert";
+        keymap_cursor = {
+          vim_insert = "blink-bar";
+          vim_normal = "steady-block";
+        };
+        enter_accept = true;
       };
-      enter_accept = true;
+      daemon.enable = true;
     };
-    daemon.enable = true;
+
+    fzf.historyWidget.command = "";
   };
 }
