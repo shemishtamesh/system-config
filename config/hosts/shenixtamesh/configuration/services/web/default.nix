740c669d87bd58c20fb3c5371491a0ba3d39880e
shemishtamesh <shemishtamail@gmail.com>
2026-07-03 16:00:33 +0300

    os config switch failed

--- config/hosts/shenixtamesh/configuration/services/web/default.nix
+++ config/hosts/shenixtamesh/configuration/services/web/default.nix
@@ -23,7 +23,7 @@
       ];
     };
     open-webui = {
-      enable = true;
+      enable = false; # TODO: currently builds a dep from source, takes all ram, and fails
       package = stable-pkgs.open-webui;
       environment = {
         ANONYMIZED_TELEMETRY = "False";
