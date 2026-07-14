46f8391a4eaa9e32d8e4d226b52b72f5312878ad
shemishtamesh <shemishtamail@gmail.com>
2026-07-14 07:37:15 +0300

    before formatting

--- config/users/shemishtamesh/packages/voxtype.nix
+++ config/users/shemishtamesh/packages/voxtype.nix
@@ -4,17 +4,13 @@
   services.voxtype = {
     enable = true;
 
-    services.voxtype = {
-      enable = true;
+    package = inputs.voxtype.packages.${pkgs.stdenv.system}.onnx-cuda;
 
-      package = inputs.voxtype.packages.${pkgs.stdenv.system}.onnx-cuda;
-
-      settings = {
-        engine = "parakeet";
-        output = {
-          mode = "type";
-          fallback_to_clipboard = true;
-        };
+    settings = {
+      engine = "parakeet";
+      output = {
+        mode = "type";
+        fallback_to_clipboard = true;
       };
     };
   };
