1fa700d8f2c124f1351bea304226992e81abca6c
shemishtamesh <shemishtamail@gmail.com>
2026-08-15 15:44:17 +0300

    system switch succeeded

--- config/shared/nixpkgs_config.nix
+++ config/shared/nixpkgs_config.nix
@@ -6,6 +6,6 @@ in
   allowUnfree = true;
   inherit cudaSupport;
   permittedInsecurePackages = [
-    "electron-39.8.10" # TODO: remove this when https://github.com/NixOS/nixpkgs/issues/526914 is resolved
+    # "electron-39.8.10" # TODO: remove this when https://github.com/NixOS/nixpkgs/issues/526914 is resolved
   ];
 }
